d1b5bc8e20b792fc12a6e9885e588768b5108ff7
Mirsario <me@mirsar.io>
2020-08-11 01:19:20 +0300

    Reimplemented StonePickaxe.

---
+++ Content/Items/Tools/StonePickaxe.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using TerrariaOverhaul.Utilities.Extensions;
+
+namespace TerrariaOverhaul.Content.Items.Tools
+{
+	public class StonePickaxe : OverhaulItem
+	{
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.CopperPickaxe);
+
+			//Tool properties
+			item.tileBoost = -1;
+			//Weapon properties.
+			item.damage /= 2;
+			item.knockBack /= 2f;
+			//Use properties.
+			item.useTime = (int)(item.useTime*1.2f);
+			item.useAnimation = (int)(item.useAnimation*1.2f);
+			//Universal properties.
+			item.width = 32;
+			item.height = 32;
+			item.value = Item.sellPrice(0,0,0,5);
+		}
+		public override void AddRecipes() => this.CreateRecipe(r => r.AddIngredient(ItemID.StoneBlock,6));
+	}
+}
