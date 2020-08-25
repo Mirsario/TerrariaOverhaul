91aa5177e71a7cb82b171f8f304f1504db3b5241
Mirsario <me@mirsar.io>
2020-08-25 19:23:35 +0300

    Keeping up tags with tML. They had too much excessive and exhausting OOP.

---
+++ Common/Tags/WallTags.cs
@@ -0,0 +1,11 @@
+﻿using Terraria.ModLoader.Tags;
+using Group = Terraria.ModLoader.Tags.WallTags;
+
+namespace TerrariaOverhaul.Common.Tags
+{
+	public static class WallTags
+	{
+		/// <summary> Makes the wall flammable. </summary>
+		public static readonly TagData Flammable = ContentTags.Get<Group>(nameof(Flammable));
+	}
+}
