a82e927fca5e639279300aa36a42780b1c75e1b2
Mirsario <me@mirsar.io>
2020-09-07 17:13:20 +0300

    Added some extensions.

---
+++ Utilities/Extensions/TileExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.DataStructures;
+
+namespace TerrariaOverhaul.Utilities.Extensions
+{
+	public static class TileExtensions
+	{
+		public static bool TryGet(this Tile[,] tiles,Point16 pos,out Tile tile) => TryGet(tiles,pos.X,pos.Y,out tile);
+		public static bool TryGet(this Tile[,] tiles,int x,int y,out Tile tile)
+		{
+			if(x>=0 && y>=0 && x<Main.maxTilesX && y<Main.maxTilesY) {
+				return (tile = tiles[x,y])!=null;
+			}
+
+			tile = null;
+
+			return false;
+		}
+	}
+}
---
+++ Utilities/Extensions/Vector2Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+
+namespace TerrariaOverhaul.Utilities.Extensions
+{
+	public static class Vector2Extensions
+	{
+		public static bool IsInWorld(this Vector2 vec) => vec.X>0 && vec.Y>0 && vec.X<(Main.maxTilesX-1)*16f && vec.Y<(Main.maxTilesY-1)*16f;
+	}
+}
