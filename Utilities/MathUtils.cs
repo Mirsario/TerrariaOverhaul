64974c0464da99e5f75d407ea49dd3483d7a80e2
Mirsario <me@mirsar.io>
2020-09-09 18:56:19 +0300

    MathUtils class.

---
+++ Utilities/MathUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace TerrariaOverhaul.Utilities
+{
+	public static class MathUtils
+	{
+		public static float Clamp01(float value) => value<=0f ? 0f : (value>=1f ? 1f : value);
+
+		public static float StepTowards(float value,float goal,float step)
+		{
+			if(goal>value) {
+				value += step;
+
+				if(value>goal) {
+					return goal;
+				}
+			} else if(goal<value) {
+				value -= step;
+
+				if(value<goal) {
+					return goal;
+				}
+			}
+
+			return value;
+		}
+	}
+}
