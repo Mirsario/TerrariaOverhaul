9316930b50b9f3a922965bb9efd3aa60f2036557
Mirsario <me@mirsar.io>
2020-09-09 23:07:41 +0300

    Reimplemented bunnyhopping. Also, autoJump is now always active.

---
+++ Common/ModEntities/Players/PlayerBunnyhopping.cs
@@ -0,0 +1,28 @@
+﻿using TerrariaOverhaul.Utilities.Extensions;
+
+namespace TerrariaOverhaul.Common.ModEntities.Players
+{
+	public class PlayerBunnyhopping : OverhaulPlayer
+	{
+		public static float DefaultBoost => 0.75f;
+
+		public float boost;
+
+		public override void ResetEffects()
+		{
+			boost = DefaultBoost;
+
+			player.autoJump = true;
+		}
+
+		public override void PostItemCheck()
+		{
+			bool onGround = player.OnGround();
+			bool wasOnGround = player.WasOnGround();
+
+			if(!onGround && wasOnGround && player.velocity.Y<0f) {
+				player.velocity.X += player.controlLeft ? -boost : player.controlRight ? boost : 0f;
+			}
+		}
+	}
+}
