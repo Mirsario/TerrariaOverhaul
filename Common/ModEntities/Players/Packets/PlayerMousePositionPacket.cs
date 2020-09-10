0237e75ec94c0698501f8f5ff35cc9a4629b2204
Mirsario <me@mirsar.io>
2020-09-10 22:06:13 +0300

    Synchronized player mouse look.

---
+++ Common/ModEntities/Players/Packets/PlayerMousePositionPacket.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.ID;
+using TerrariaOverhaul.Core.Systems.Networking;
+using TerrariaOverhaul.Utilities.Extensions;
+
+namespace TerrariaOverhaul.Common.ModEntities.Players.Packets
+{
+	public class PlayerMousePositionPacket : NetPacket
+	{
+		public PlayerMousePositionPacket(Player player) : base(w => {
+			var modPlayer = player.GetModPlayer<PlayerDirectioning>();
+
+			w.TryWriteSenderPlayer(player);
+
+			w.WriteVector2(modPlayer.mouseWorld);
+		})
+		{ }
+
+		public override void Read(BinaryReader reader,int sender)
+		{
+			if(!reader.TryReadSenderPlayer(sender,out var player)) {
+				return;
+			}
+
+			var modPlayer = player.GetModPlayer<PlayerDirectioning>();
+
+			modPlayer.mouseWorld = reader.ReadVector2();
+
+			//Resend
+			if(Main.netMode==NetmodeID.Server) {
+				MultiplayerSystem.SendPacket(new PlayerMousePositionPacket(player),ignoreClient: sender);
+			}
+		}
+	}
+}
--- Common/ModEntities/Players/PlayerDirectioning.cs
+++ Common/ModEntities/Players/PlayerDirectioning.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using TerrariaOverhaul.Common.ModEntities.Players.Packets;
+using TerrariaOverhaul.Core.Systems.Networking;
 using TerrariaOverhaul.Utilities.Extensions;
 
 namespace TerrariaOverhaul.Common.ModEntities.Players
 {
 	public sealed class PlayerDirectioning : ModPlayer
 	{
+		private const int MouseWorldSyncFrequency = 12;
+
 		public int forcedDirection;
 		public Vector2 mouseWorld;
+
+		private Vector2 lastSyncedMouseWorld;
 
 		public override void PreUpdate() => SetDirection();
 		public override void PostUpdate() => SetDirection();
@@ -31,8 +38,14 @@ namespace TerrariaOverhaul.Common.ModEntities.Players
 				return;
 			}
 
-			if(player.IsLocal()) {
+			if(player.IsLocal() && Main.hasFocus) {
 				mouseWorld = Main.MouseWorld;
+
+				if(Main.netMode==NetmodeID.MultiplayerClient && Main.GameUpdateCount%MouseWorldSyncFrequency==0 && lastSyncedMouseWorld!=mouseWorld) {
+					MultiplayerSystem.SendPacket(new PlayerMousePositionPacket(player));
+
+					lastSyncedMouseWorld = mouseWorld;
+				}
 			}
 
 			if(!player.pulley && (!player.mount.Active || player.mount.AllowDirectionChange)) {
