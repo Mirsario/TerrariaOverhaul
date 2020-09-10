4e1a502ddbd62c959741fea1cffc21e71e0b1aa7
Mirsario <me@mirsar.io>
2020-09-10 16:57:40 +0300

    Reimplemented forcing of animation frames.

---
+++ Common/ModEntities/Players/PlayerAnimations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TerrariaOverhaul.Utilities.Enums;
+using TerrariaOverhaul.Utilities.Extensions;
+
+namespace TerrariaOverhaul.Common.ModEntities.Players
+{
+	public sealed class PlayerAnimations : OverhaulPlayer
+	{
+		private const int PlayerSheetWidth = 40;
+		private const int PlayerSheetHeight = 56;
+
+		public PlayerFrames? forcedHeadFrame;
+		public PlayerFrames? forcedBodyFrame;
+		public PlayerFrames? forcedLegFrame;
+
+		public override void PostUpdate()
+		{
+			void TryForceFrame(ref Rectangle frame,ref PlayerFrames? newFrame)
+			{
+				if(newFrame.HasValue) {
+					frame = new Rectangle(0,PlayerSheetHeight*(int)newFrame.Value,PlayerSheetWidth,PlayerSheetHeight);
+
+					newFrame = null;
+				}
+			}
+
+			TryForceFrame(ref player.headFrame,ref forcedHeadFrame);
+			TryForceFrame(ref player.bodyFrame,ref forcedBodyFrame);
+			TryForceFrame(ref player.legFrame,ref forcedLegFrame);
+		}
+	}
+}
