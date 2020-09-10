cbf9a4f0521fc8e9e75c31a7b516aa060eccc1a7
Mirsario <me@mirsar.io>
2020-09-10 22:07:00 +0300

    Reimplemented item rotation forcing.

---
+++ Common/ModEntities/Players/PlayerItemRotation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TerrariaOverhaul.Utilities.Extensions;
+
+namespace TerrariaOverhaul.Common.ModEntities.Players
+{
+	public sealed class PlayerItemRotation : OverhaulPlayer
+	{
+		public float? forcedItemRotation;
+
+		public override void PostUpdate()
+		{
+			if(forcedItemRotation.HasValue) {
+				player.itemRotation = forcedItemRotation.Value;
+
+				forcedItemRotation = null;
+			}
+		}
+	}
+}
