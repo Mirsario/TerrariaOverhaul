cec70796669ff3ef59a9f556250b7db9fee8dfb3
Mirsario <me@mirsar.io>
2020-09-10 13:46:18 +0300

    Partially reimplemented InputSystem

---
+++ Core/Systems/Input/InputSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Core.Systems.Input
+{
+	public sealed class InputSystem : ModSystem
+	{
+		public static ModHotKey KeyReload { get; private set; }
+		public static ModHotKey KeyDodgeroll { get; private set; }
+		public static ModHotKey KeyEmoteWheel { get; private set; }
+		public static ModHotKey KeyQuickAction { get; private set; }
+		public static ModHotKey KeySwitchQuickItem { get; private set; }
+
+		public override void Load()
+		{
+			KeyReload = Mod.RegisterHotKey("Reload","R");
+			KeyDodgeroll = Mod.RegisterHotKey("Dodgeroll","LeftControl");
+			KeyEmoteWheel = Mod.RegisterHotKey("Emote Wheel","Q");
+			KeyQuickAction = Mod.RegisterHotKey("Quick Action","F");
+			KeySwitchQuickItem = Mod.RegisterHotKey("Switch Quick Item","X");
+		}
+	}
+}
