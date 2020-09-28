5b4bde8575f975efc942ed2087dadd128acf4bce
Mirsario <me@mirsar.io>
2020-09-28 22:40:08 +0300

    Added a ModMenu.

---
+++ Common/MenuStyles/OverhaulMenu.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Common.MenuStyles
+{
+	public sealed class OverhaulMenu : ModMenu
+	{
+		public override Asset<Texture2D> Logo => Mod.GetTexture("Common/MenuStyles/Logo");
+	}
+}
