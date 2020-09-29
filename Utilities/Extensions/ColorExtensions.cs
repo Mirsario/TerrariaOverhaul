0ae0bcdb79d481a705745160d7580a1abc67612d
Mirsario <me@mirsar.io>
2020-09-29 17:20:54 +0300

    Added StringUtils and ColorExtensions

---
+++ Utilities/Extensions/ColorExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace TerrariaOverhaul.Utilities.Extensions
+{
+    internal static class ColorExtensions
+    {
+        public static Color WithAlpha(this Color color,byte alpha)
+        {
+            color.A = alpha;
+
+            return color;
+        }
+        public static Color WithAlpha(this Color color,float alpha)
+        {
+            color.A = (byte)(MathHelper.Clamp(alpha,0f,1f)*255f);
+
+            return color;
+        }
+
+        public static float GetHue(this Color color)
+        {
+            if(color.R==color.G && color.G==color.B) {
+                return 0f;
+            }
+
+            float r = color.R/255.0f;
+            float g = color.G/255.0f;
+            float b = color.B/255.0f;
+
+            float max, min;
+            float delta;
+            float hue = 0.0f;
+
+            max = Math.Max(r,Math.Max(g,b));
+            min = Math.Min(r,Math.Min(g,b));
+
+            delta = max-min;
+
+            if(r ==max) {
+                hue = (g - b) / delta;
+            } else if(g == max) {
+                hue = 2 + (b - r) / delta;
+            } else if(b == max) {
+                hue = 4 + (r - g) / delta;
+            }
+
+            hue *= 60;
+
+            if(hue < 0.0f) {
+                hue += 360.0f;
+            }
+
+            return hue;
+        }
+
+        public static string ToHexRGB(this Color color) => BitConverter.ToString(new byte[] { color.R,color.G,color.B }).Replace("-","");
+        public static string ToHexRGBA(this Color color) => BitConverter.ToString(new byte[] { color.R,color.G,color.B,color.A }).Replace("-","");
+        public static string ToHexARGB(this Color color) => BitConverter.ToString(new byte[] { color.A,color.R,color.G,color.B }).Replace("-","");
+    }
+}
---
+++ Utilities/StringUtils.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TerrariaOverhaul.Utilities.Extensions;
+
+namespace TerrariaOverhaul.Utilities
+{
+	internal static class StringUtils
+	{
+		private static readonly Dictionary<string,string> regexCache = new Dictionary<string,string>();
+
+		public static string SafeFormat(string str,object arg0) => str?.Replace("{0}",arg0?.ToString() ?? "");
+		public static string SafeFormat(string str,object arg0,object arg1) => str?.Replace("{0}",arg0?.ToString() ?? "").Replace("{1}",arg1?.ToString() ?? "");
+		public static string SafeFormat(string str,params object[] args)
+		{
+			if(str!=null) {
+				for(int i = 0;i<args.Length;i++) {
+					str = str.Replace("{"+i+"}",args[i]?.ToString() ?? "");
+				}
+			}
+
+			return str;
+		}
+
+		public static string JoinLines(params string[] strings) => string.Join("\r\n",strings);
+		public static string ColoredText(Color color,string text) => $"[c/{color.ToHexRGB()}:{text}]";
+
+		public static string GetTextWithoutTags(string colorCodedText)
+		{
+			if(!regexCache.TryGetValue(colorCodedText,out string plainText)) {
+				regexCache[colorCodedText] = plainText = Regex.Replace(colorCodedText,@"\[\w\/\w+:([^\]]+)\]","$1");
+			}
+
+			return plainText;
+		}
+	}
+}
