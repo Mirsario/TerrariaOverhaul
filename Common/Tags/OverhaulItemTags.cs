a0f0910bd2ed43731a74c6c7f6c62913ed269907
Mirsario <me@mirsar.io>
2020-10-24 19:12:51 +0300

    Added ClimbingClaws, ShoeSpikes and NinjaGear item tags.

---
+++ Common/Tags/OverhaulItemTags.cs
@@ -0,0 +1,46 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Tags;
+using TerrariaOverhaul.Utilities.Extensions;
+using Group = Terraria.ModLoader.Tags.ItemTags;
+
+namespace TerrariaOverhaul.Common.Tags
+{
+	public sealed class OverhaulItemTags : ILoadable
+	{
+		/// <summary> Speeds up climbing. </summary>
+		public static readonly TagData ClimbingClaws = ContentTags.Get<Group>(nameof(ClimbingClaws));
+
+		/// <summary> Increases stomping damage. </summary>
+		public static readonly TagData ShoeSpikes = ContentTags.Get<Group>(nameof(ShoeSpikes));
+
+		/// <summary> Enables walljumping. </summary>
+		public static readonly TagData NinjaGear = ContentTags.Get<Group>(nameof(NinjaGear));
+
+		void ILoadable.Load(Mod mod)
+		{
+			ClimbingClaws.SetMultiple(
+				ItemID.ClimbingClaws,
+				ItemID.TigerClimbingGear,
+				ItemID.MasterNinjaGear
+			);
+
+			ShoeSpikes.SetMultiple(
+				ItemID.ShoeSpikes,
+				ItemID.TigerClimbingGear,
+				ItemID.MasterNinjaGear
+			);
+
+			NinjaGear.SetMultiple(
+				ItemID.ClimbingClaws,
+				ItemID.ShoeSpikes,
+				ItemID.TigerClimbingGear,
+				ItemID.MasterNinjaGear
+			);
+		}
+		void ILoadable.Unload()
+		{
+
+		}
+	}
+}
