7bf003621a407f6cd5997b8e7bf449e155560cbf
Mirsario <me@mirsar.io>
2020-10-24 19:12:37 +0300

    Reimplemented some utils and extensions.

--- Utilities/Extensions/PlayerExtensions.cs
+++ Utilities/Extensions/PlayerExtensions.cs
@@ -1,9 +1,13 @@
-﻿using Terraria;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Terraria;
 
 namespace TerrariaOverhaul.Utilities.Extensions
 {
 	public static class PlayerExtensions
 	{
+		//Essentials
+
 		public static bool IsLocal(this Player player) => player.whoAmI == Main.myPlayer;
 
 		public static bool OnGround(this Player player) => player.velocity.Y == 0f; //player.GetModPlayer<PlayerMovement>().OnGround;
@@ -11,21 +15,55 @@ namespace TerrariaOverhaul.Utilities.Extensions
 
 		public static int KeyDirection(this Player player) => player.controlLeft ? -1 : player.controlRight ? 1 : 0;
 
+		//Inventory
+
+		public static bool HasAccessory(this Player player, int itemId) => player.EnumerateAccessories().Any(tuple => tuple.item.type == itemId);
+		public static bool HasAccessory(this Player player, bool any, params int[] itemIds)
+		{
+			var accessories = player.EnumerateAccessories();
+
+			bool Predicate((Item,int) tuple) => itemIds.Contains(tuple.Item1.type);
+
+			return any ? accessories.Any(Predicate) : accessories.All(Predicate);
+		}
+
+		public static IEnumerable<(Item item, int index)> EnumerateAccessories(this Player player)
+		{
+			for(int i = 3; i < 8 + player.extraAccessorySlots; i++) {
+				var item = player.armor[i];
+
+				if(item != null && item.active) {
+					yield return (item, i);
+				}
+			}
+		}
+
+		//Grappling hooks
+
 		public static void StopGrappling(this Player player, Projectile exceptFor = null)
+		{
+			foreach(var (grapplingHook, hookIndex) in player.EnumerateGrapplingHooks()) {
+				if(grapplingHook != exceptFor && grapplingHook.ai[0] == 2f) {
+					grapplingHook.Kill();
+
+					player.grappling[hookIndex] = -1;
+				}
+			}
+		}
+
+		public static IEnumerable<(Projectile projectile, int hookIndex)> EnumerateGrapplingHooks(this Player player)
 		{
 			for(int i = 0; i < player.grappling.Length; i++) {
 				int grapplingHookId = player.grappling[i];
 
 				if(grapplingHookId < 0) {
-					continue;
+					break;
 				}
 
 				var grapplingHook = Main.projectile[grapplingHookId];
 
-				if(grapplingHook != null && grapplingHook.active && grapplingHook != exceptFor && grapplingHook.ai[0] == 2f) {
-					grapplingHook.Kill();
-
-					player.grappling[i] = -1;
+				if(grapplingHook != null && grapplingHook.active) {
+					yield return (grapplingHook, i);
 				}
 			}
 		}
---
+++ Utilities/TileCheckUtils.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using TerrariaOverhaul.Utilities.Extensions;
+
+namespace TerrariaOverhaul.Utilities
+{
+	public static class TileCheckUtils
+	{
+		public static bool CheckIfAllBlocksAreSolid(int x, int y, int width, int height)
+		{
+			for(int yy = 0; yy < height; yy++) {
+				for(int xx = 0; xx < width; xx++) {
+					if(!Main.tile.TryGet(x + xx, y + yy, out Tile tile) || !Main.tileSolid[tile.type] || !tile.active()) {
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+}
