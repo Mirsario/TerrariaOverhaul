43cba616662eb7dd7306d8773e2d29c8331869fd
Mirsario <me@mirsar.io>
2020-10-27 01:08:01 +0300

    Reimplemented gore floating and a fix for them getting stuck.

--- Common/Systems/Gores/OverhaulGore.cs
+++ Common/Systems/Gores/OverhaulGore.cs
@@ -2,10 +2,16 @@
 using System;
 using Terraria;
 using Terraria.Audio;
+using Terraria.Graphics.Effects;
 using Terraria.GameContent;
+using Terraria.GameContent.Shaders;
+using Terraria.ID;
 using Terraria.ModLoader;
+using TerrariaOverhaul.Common.Systems.Fires;
 using TerrariaOverhaul.Content.SimpleEntities;
 using TerrariaOverhaul.Core.Systems.SimpleEntities;
+using TerrariaOverhaul.Utilities.Extensions;
+using Terraria.DataStructures;
 
 namespace TerrariaOverhaul.Common.Systems.Gores
 {
@@ -18,18 +24,17 @@ namespace TerrariaOverhaul.Common.Systems.Gores
 		//public bool noBlood;
 		//public bool noFallSounds;
 		//public bool wasInLiquid;
-		public int width;
-		public int height;
-		public int scaledWidth;
-		public int scaledHeight;
-		public int minScaledDimm;
+		public bool stopBleeding;
+		public Vector2 size;
+		public Vector2 originalSize;
+		public float minDimension;
 		//public int time;
 		//public float health;
 		public Vector2 prevVelocity;
 		public Vector2 prevPosition;
 		public Color? bleedColor;
 
-		//public GorePreset preset;
+		public Vector2 Center => position + size * 0.5f;
 
 		//Load-time
 		public void Load(Mod mod) { }
@@ -41,28 +46,97 @@ namespace TerrariaOverhaul.Common.Systems.Gores
 
 			var texture = TextureAssets.Gore[type].Value;
 
-			width = texture.Width;
-			height = texture.Height / Frame.ColumnCount;
-			scaledWidth = (int)(width * scale);
-			scaledHeight = (int)(height * scale);
-			minScaledDimm = Math.Min(scaledWidth, scaledHeight);
+			originalSize = new Vector2(texture.Width, texture.Height / Frame.ColumnCount);
+			size = originalSize * scale;
+			minDimension = Math.Min(size.X, size.Y);
 		}
 		public void PostUpdate()
 		{
-			var goreCenter = new Vector2(
-				position.X + scaledWidth * 0.5f,
-				position.Y + scaledHeight * 0.5f
-			);
+			var center = Center;
+			var point = center.ToTileCoordinates16();
 
-			//Bleeding
-			if(bleedColor.HasValue && Main.GameUpdateCount % 6 == 0) { // && velocity.Length() >= 0.5f && prevVelocity.Length() >= 0.5f) {
-				SimpleEntity.Instantiate<BloodParticle>(p => {
-					p.position = goreCenter + Main.rand.NextVector2Square(-4f, 4f);
-					p.velocity = Vector2.Transform(Vector2.UnitX * 3f, Matrix.CreateRotationZ(rotation + MathHelper.ToRadians(Main.rand.NextFloat(-10f, 10f))));
-					p.color = bleedColor.Value;
-				});
+			if(!Main.tile.TryGet(point, out var tile)) {
+				active = false;
+
+				return;
 			}
 
+			if(Main.tileSolid[tile.type] && tile.active()) {
+				MoveGoreUpwards(point);
+			} else if(tile.liquid > 0) {
+				OnLiquidCollision(tile);
+			}
+
+			Bleeding();
+			Bouncing();
+
+			prevVelocity = velocity;
+			prevPosition = position;
+		}
+		public void CopyFrom(Gore gore)
+		{
+			type = gore.type;
+			light = gore.light;
+			Frame = gore.Frame;
+			alpha = gore.alpha;
+			scale = gore.scale;
+			sticky = gore.sticky;
+			active = gore.active;
+			rotation = gore.rotation;
+			velocity = gore.velocity;
+			position = gore.position;
+			timeLeft = gore.timeLeft;
+			drawOffset = gore.drawOffset;
+			behindTiles = gore.behindTiles;
+			frameCounter = gore.frameCounter;
+		}
+		public Vector2 GetRandomPoint() => position + Main.rand.NextVector2Square(size.X, size.Y);
+
+		private void OnLiquidCollision(Tile tile)
+		{
+			//Evaporate in lava
+			if(tile.lava()) {
+				SoundEngine.PlaySound(FireSystem.ExtinguishSound, position);
+				
+				for(int i = 0; i < 5; i++) {
+					Dust.NewDustPerfect(GetRandomPoint(), DustID.Smoke, Main.rand.NextVector2(-1f, -1f, 1f, 0f), 128, Color.White);
+				}
+
+				active = false;
+
+				return;
+			}
+
+			//Water physics
+			var center = Center;
+			float yPosRelative = center.Y - (float)(Math.Floor(center.Y / 16f) * 16f);
+
+			if(yPosRelative > 16f - tile.liquid / 255f * 16f) {
+				velocity.X = MathHelper.Lerp(velocity.X, 0f, 0.5f / 60f);
+				velocity.Y = MathHelper.Lerp(velocity.Y, -3f, 5f / 60f);
+
+				//Create ripples
+				if(prevVelocity.Length() >= 2f && Main.GameUpdateCount % 5 == 0) {
+					((WaterShaderData)Filters.Scene["WaterDistortion"].GetShader()).QueueRipple(center, minDimension / 32f);
+				}
+
+				stopBleeding = true;
+			}
+		}
+		private void Bleeding()
+		{
+			if(stopBleeding || !bleedColor.HasValue || Main.GameUpdateCount % 5 != 0 || velocity.Length() < 0.5f || prevVelocity.Length() < 0.5f) {
+				return;
+			}
+
+			SimpleEntity.Instantiate<BloodParticle>(p => {
+				p.position = GetRandomPoint();
+				p.velocity = Vector2.Transform(Vector2.UnitX * 3f, Matrix.CreateRotationZ(rotation + MathHelper.ToRadians(Main.rand.NextFloat(-10f, 10f))));
+				p.color = bleedColor.Value;
+			});
+		}
+		private void Bouncing()
+		{
 			if(velocity.Y == 0f) {
 				//Vertical bouncing
 				if(Math.Abs(prevVelocity.Y) >= 1f) {
@@ -85,26 +159,15 @@ namespace TerrariaOverhaul.Common.Systems.Gores
 
 				velocity.X = -prevVelocity.X * 0.66f;
 			}
-
-			prevVelocity = velocity;
-			prevPosition = position;
 		}
-		public void CopyFrom(Gore gore)
+		private void MoveGoreUpwards(Point16 point)
 		{
-			type = gore.type;
-			light = gore.light;
-			Frame = gore.Frame;
-			alpha = gore.alpha;
-			scale = gore.scale;
-			sticky = gore.sticky;
-			active = gore.active;
-			rotation = gore.rotation;
-			velocity = gore.velocity;
-			position = gore.position;
-			timeLeft = gore.timeLeft;
-			drawOffset = gore.drawOffset;
-			behindTiles = gore.behindTiles;
-			frameCounter = gore.frameCounter;
+			int upY = point.Y - 2;
+
+			if(upY >= 0 && Main.tile.TryGet(point.X, upY, out var upTile) && (!Main.tileSolid[upTile.type] || !upTile.active())) {
+				position.Y -= 1;
+				velocity.Y = 0.000001f;
+			}
 		}
 	}
 }
