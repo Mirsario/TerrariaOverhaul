0b4482a0194e4c0349a8c2268ee0a6f42166e9b2
Mirsario <me@mirsar.io>
2020-10-27 01:06:44 +0300

    Added Extinguish sound and an empty FireSystem to hold it.

---
+++ Common/Systems/Fires/FireSystem.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Common.Systems.Fires
+{
+	public class FireSystem : ModSystem
+	{
+		public static readonly SoundStyle ExtinguishSound = new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Fire/Extinguish", 0, pitchVariance: 0.1f);
+	}
+}
