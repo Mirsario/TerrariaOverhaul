1e9d3acbd9b6af686a6b8fa02bdffe637134e392
Mirsario <me@mirsar.io>
2020-10-27 01:06:28 +0300

    Added UnifiedRandom.NextVector2(float, float, float, float) extension.

---
+++ Utilities/Extensions/RandomExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Utilities;
+
+namespace TerrariaOverhaul.Utilities.Extensions
+{
+	public static class RandomExtensions
+	{
+		public static Vector2 NextVector2(this UnifiedRandom random, float minX, float minY, float maxX, float maxY) => new Vector2(
+			random.NextFloat(minX, maxX),
+			random.NextFloat(minY, maxY)
+		);
+	}
+}
