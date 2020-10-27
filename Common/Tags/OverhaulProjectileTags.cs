7c24d61fa7f2b6df83466e5d5eded79b615262a1
Mirsario <me@mirsar.io>
2020-10-27 22:00:49 +0300

    Added Incediary and Extinguisher projectile tags.

---
+++ Common/Tags/OverhaulProjectileTags.cs
@@ -0,0 +1,90 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Tags;
+using TerrariaOverhaul.Utilities.Extensions;
+using Group = Terraria.ModLoader.Tags.ItemTags;
+
+namespace TerrariaOverhaul.Common.Tags
+{
+	public sealed class OverhaulProjectileTags : ILoadable
+	{
+		/// <summary> These set things on fire. Enough said. </summary>
+		public static readonly TagData Incendiary = ContentTags.Get<Group>(nameof(Incendiary));
+
+		/// <summary> Projectiles with this tag extinguish fires and interact with incendiary projectiles. </summary>
+		public static readonly TagData Extinguisher = ContentTags.Get<Group>(nameof(Extinguisher));
+
+		void ILoadable.Load(Mod mod)
+		{
+			Incendiary.SetMultiple(
+				ProjectileID.Spark,
+				ProjectileID.FlamesTrap,
+				ProjectileID.GreekFire1,
+				ProjectileID.GreekFire2,
+				ProjectileID.GreekFire3,
+				ProjectileID.MolotovCocktail,
+				ProjectileID.MolotovFire,
+				ProjectileID.MolotovFire2,
+				ProjectileID.MolotovFire3,
+				ProjectileID.CultistBossFireBall,
+				ProjectileID.DD2BetsyFireball,
+				ProjectileID.DD2BetsyFlameBreath,
+				ProjectileID.InfernoFriendlyBlast,
+				ProjectileID.InfernoHostileBlast,
+				ProjectileID.Fireball,
+				ProjectileID.BallofFire,
+				ProjectileID.Flamelash,
+				ProjectileID.Flames,
+				ProjectileID.FireArrow,
+				ProjectileID.Meteor1,
+				ProjectileID.Meteor2,
+				ProjectileID.Meteor3,
+				ProjectileID.Flare
+			);
+
+			Extinguisher.SetMultiple(
+				ProjectileID.WaterStream,
+				ProjectileID.WaterBolt,
+				ProjectileID.HolyWater,
+				ProjectileID.UnholyWater,
+				ProjectileID.BloodWater,
+				ProjectileID.WaterGun,
+				ProjectileID.CultistBossIceMist,
+				ProjectileID.IceBolt,
+				ProjectileID.IceBlock,
+				ProjectileID.IceBoomerang,
+				ProjectileID.IceSpike,
+				ProjectileID.IcewaterSpit,
+				ProjectileID.IceSickle,
+				ProjectileID.BallofFrost,
+				ProjectileID.FrostArrow,
+				ProjectileID.FrostBeam,
+				ProjectileID.FrostBlastFriendly,
+				ProjectileID.FrostBlastHostile,
+				ProjectileID.FrostBoltStaff,
+				ProjectileID.FrostBoltSword,
+				ProjectileID.FrostburnArrow,
+				ProjectileID.FrostDaggerfish,
+				ProjectileID.FrostHydra,
+				ProjectileID.FrostShard,
+				ProjectileID.FrostWave,
+				ProjectileID.GoldenShowerFriendly,
+				ProjectileID.GoldenShowerHostile,
+				ProjectileID.BabySnowman,
+				ProjectileID.NorthPoleSnowflake,
+				ProjectileID.SnowBallFriendly,
+				ProjectileID.SnowBallHostile,
+				ProjectileID.BlueFlare,
+				ProjectileID.BloodRain,
+				ProjectileID.BloodCloudMoving,
+				ProjectileID.BloodCloudRaining,
+				ProjectileID.BloodWater,
+				ProjectileID.RainCloudMoving,
+				ProjectileID.RainCloudRaining,
+				ProjectileID.RainFriendly,
+				ProjectileID.RainNimbus
+			);
+		}
+		void ILoadable.Unload() { }
+	}
+}
