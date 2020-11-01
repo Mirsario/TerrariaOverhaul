71e284fa394ff208efedd184b7da959adcb6085b
Mirsario <me@mirsar.io>
2020-11-01 16:00:51 +0300

    Added MathUtils.Clamp int & float methods.

--- Utilities/MathUtils.cs
+++ Utilities/MathUtils.cs
@@ -2,6 +2,8 @@
 {
 	public static class MathUtils
 	{
+		public static int Clamp(int value, int min, int max) => value <= min ? min : (value >= max ? max : value);
+		public static float Clamp(float value, float min, float max) => value <= min ? min : (value >= max ? max : value);
 		public static float Clamp01(float value) => value <= 0f ? 0f : (value >= 1f ? 1f : value);
 
 		public static float StepTowards(float value, float goal, float step)
