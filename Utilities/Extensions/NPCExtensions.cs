f1a88d3cd62f1ff141f40cf1da272806c3ba4cf0
Mirsario <me@mirsar.io>
2020-11-10 17:13:40 +0300

    Reimplemented fighter AI jumping at their targets.

---
+++ Utilities/Extensions/NPCExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+
+namespace TerrariaOverhaul.Utilities.Extensions
+{
+	public static class NpcExtensions
+	{
+		public static Entity GetTarget(this NPC npc)
+		{
+			if(!npc.HasValidTarget) {
+				return null;
+			}
+
+			return npc.HasPlayerTarget ? Main.player[npc.target] : (Entity)Main.npc[npc.target - 300];
+		}
+	}
+}
