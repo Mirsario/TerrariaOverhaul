16de3a152f30b30bc8058d9173832a36d1ef73df
Mirsario <me@mirsar.io>
2020-11-11 13:22:42 +0300

    Sketched ItemOverhauls via GlobalItem with 1.4_conditionalinstancing feature.

---
+++ Common/ModEntities/Items/Overhauls/Generic/BroadswordOverhaul.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls.Generic
+{
+	public class BroadswordOverhaul : ItemOverhaul
+	{
+		public override bool ShouldApplyItemOverhaul(Item item)
+		{
+			//Broadswords always swing, deal melee damage, don't have channeling, and are visible
+			if(item.useStyle != ItemUseStyleID.Swing || item.noMelee || item.channel || item.noUseGraphic) {
+				return false;
+			}
+
+			//Avoid tools and blocks
+			if(item.pick != 0 || item.axe != 0 || item.hammer != 0 || item.createTile >= 0 || item.createWall >= 0) {
+				return false;
+			}
+
+			if(item.DamageType != DamageClass.Melee) {
+				return false;
+			}
+
+			return true;
+		}
+
+		public override bool UseItem(Item item, Player player)
+		{
+			//Main.NewText("Swinging a broadsword.");
+
+			return true;
+		}
+	}
+}
---
+++ Common/ModEntities/Items/Overhauls/ItemOverhaul.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls
+{
+	public abstract class ItemOverhaul : GlobalItem
+	{
+		private static List<ItemOverhaul> itemOverhauls;
+
+		protected Item item;
+
+		public override bool InstancePerEntity => true;
+
+		public abstract bool ShouldApplyItemOverhaul(Item item);
+
+		public override bool InstanceForEntity(Item item) => ChooseItemOverhaul(item) == this;
+		//
+		public override void Load()
+		{
+			if(itemOverhauls == null) {
+				itemOverhauls = new List<ItemOverhaul>();
+			}
+
+			itemOverhauls.Add(this);
+		}
+		public override void Unload()
+		{
+			if(itemOverhauls != null) {
+				itemOverhauls.Clear();
+
+				itemOverhauls = null;
+			}
+		}
+		public override GlobalItem Clone(Item item, Item itemClone)
+		{
+			var clone = base.Clone(item, itemClone);
+
+			((ItemOverhaul)clone).item = itemClone;
+
+			return clone;
+		}
+
+		public static ItemOverhaul ChooseItemOverhaul(Item item)
+		{
+			//May need some sort of priority system in the future. And cache?
+			for(int i = 0; i < itemOverhauls.Count; i++) {
+				var itemOverhaul = itemOverhauls[i];
+
+				if(itemOverhaul.ShouldApplyItemOverhaul(item)) {
+					return itemOverhaul;
+				}
+			}
+
+			return null;
+		}
+	}
+}
