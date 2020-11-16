d2a37789167d5323403cb2cd54d931cbcca55267
Mirsario <me@mirsar.io>
2020-11-16 19:43:55 +0300

    Forced on autoreuse for all items, at the expense of 2 ticks of speed.

---
+++ Common/ModEntities/Items/ItemAutoSwing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Common.ModEntities.Items
+{
+	public class ItemAutoSwing : GlobalItem
+	{
+		public override void SetDefaults(Item item)
+		{
+			if(!item.autoReuse) {
+				item.autoReuse = true;
+				item.useTime += 2;
+				item.useAnimation += 2;
+			}
+		}
+	}
+}
