806bedc9f395a9b9b4ab98f95cb50874c8fc2adb
Mirsario <me@mirsar.io>
2021-02-26 20:20:14 +0300

    Temporary melee audio implementation.

--- Common/ModEntities/Items/Overhauls/Generic/MeleeWeapon.cs
+++ Common/ModEntities/Items/Overhauls/Generic/MeleeWeapon.cs
@@ -1,12 +1,38 @@
-﻿using Terraria;
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Common.SoundStyles;
+using TerrariaOverhaul.Utilities;
 
 namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls.Generic
 {
 	public abstract class MeleeWeapon : ItemOverhaul
 	{
+		public virtual float GetHeavyness(Item item)
+		{
+			float averageDimension = (item.width + item.height) * 0.5f;
+
+			const float HeaviestSpeed = 0.5f;
+			const float LightestSpeed = 5f;
+
+			float speed = 1f / (Math.Max(1, item.useAnimation) / 60f);
+			float speedResult = MathHelper.Clamp(MathUtils.InverseLerp(speed, LightestSpeed, HeaviestSpeed), 0f, 1f);
+			float sizeResult = Math.Max(0f, (averageDimension) / 10f);
+
+			float result = speedResult;
+
+			return MathHelper.Clamp(result, 0f, 1f);
+		}
+
 		public override void SetDefaults(Item item)
 		{
-			
+			item.UseSound = new BlendedSoundStyle(
+				new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Items/Melee/SwingLight", 4),
+				new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Items/Melee/SwingHeavy", 4),
+				GetHeavyness(item),
+				0.3f
+			);
 		}
 	}
 }
---
+++ Common/SoundStyles/BlendedSoundStyle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Terraria.Audio;
+
+namespace TerrariaOverhaul.Common.SoundStyles
+{
+	public class BlendedSoundStyle : SoundStyle
+	{
+		public override bool IsTrackable => true;
+
+		public SoundStyle StyleA { get; private set; }
+		public SoundStyle StyleB { get; private set; }
+		public float Factor { get; set; }
+
+		public BlendedSoundStyle(SoundStyle styleA, SoundStyle styleB, float factor, float volume = 1f) : base(volume, 0f, 0f, SoundType.Sound)
+		{
+			StyleA = styleA;
+			StyleB = styleB;
+			Factor = factor;
+		}
+
+		//TODO: This is a very temporary implementation. Should improve with improvements to tModLoader:1.4_soundfix
+		public override SoundEffectInstance Play(Vector2? position)
+		{
+			var method = typeof(object).GetMethod("MemberwiseClone", BindingFlags.Instance | BindingFlags.NonPublic);
+
+			var cloneA = (SoundStyle)method.Invoke(StyleA, null);
+			var cloneB = (SoundStyle)method.Invoke(StyleB, null);
+
+			cloneA.Volume *= Volume * (1f - Factor);
+			cloneB.Volume *= Volume * Factor;
+
+			var instanceA = cloneA.Play(position);
+			var instanceB = cloneB.Play(position);
+
+			//StyleA.Volume = oldVolumeA;
+			//StyleB.Volume = oldVolumeB;
+
+			return Factor <= 0.5f ? instanceA : instanceB;
+		}
+		public override SoundEffect GetRandomSound() => throw new NotImplementedException();
+	}
+}
