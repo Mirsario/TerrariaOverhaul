06db51943af2f763b6fb6f18afee467dabc2702a
Mirsario <me@mirsar.io>
2021-03-02 20:04:33 +0300

    Command description.

--- Common/Commands/ToggleDebugDisplayCommand.cs
+++ Common/Commands/ToggleDebugDisplayCommand.cs
@@ -7,6 +7,7 @@ namespace TerrariaOverhaul.Common.Commands
 	public class ToggleDebugDisplayCommand : ModCommand
 	{
 		public override string Command => "oToggleDebugDisplay";
+		public override string Description => "Toggles Overhaul's visual debugging features";
 		public override CommandType Type => CommandType.Chat;
 
 		public override void Action(CommandCaller caller, string input, string[] args)
