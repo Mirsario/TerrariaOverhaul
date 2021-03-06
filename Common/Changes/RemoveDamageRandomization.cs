b9a19706b1ecf15194d9a2262364fbf140f4ff51
Mirsario <me@mirsar.io>
2021-03-06 15:42:09 +0300

    Reimplemented removal of damage randomization.

---
+++ Common/Changes/RemoveDamageRandomization.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Common.Changes
+{
+	public sealed class RemoveDamageRandomization : ILoadable
+	{
+		public void Load(Mod mod) => On.Terraria.Main.DamageVar += (orig, damage, luck) => (int)Math.Round(damage);
+		public void Unload() { }
+	}
+}
