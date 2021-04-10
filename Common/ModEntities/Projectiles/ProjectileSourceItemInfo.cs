5a8cfdbbb07579d3c019c4a13a178bfdbf56198c
Mirsario <me@mirsar.io>
2021-04-10 20:53:48 +0300

    Fixed ProjectileSourceItemInfo not working for projectiles spawned by other projectiles.

--- Common/ModEntities/Projectiles/ProjectileSourceItemInfo.cs
+++ Common/ModEntities/Projectiles/ProjectileSourceItemInfo.cs
@@ -8,6 +8,7 @@ namespace TerrariaOverhaul.Common.ModEntities.Projectiles
 		public bool Available { get; private set; }
 		public int UseTime { get; private set; }
 		public int UseAnimation { get; private set; }
+		public int ManaUse { get; private set; }
 
 		public override bool InstancePerEntity => true;
 
@@ -19,13 +20,28 @@ namespace TerrariaOverhaul.Common.ModEntities.Projectiles
 			On.Terraria.Projectile.NewProjectile_IProjectileSource_float_float_float_float_int_int_float_int_float_float += (orig, source, x, y, speedX, speedY, type, damage, knockback, Owner, ai0, ai1) => {
 				int id = orig(source, x, y, speedX, speedY, type, damage, knockback, Owner, ai0, ai1);
 
-				if(id != Main.maxProjectiles && source is ProjectileSource_Item itemSource) {
+				if(id != Main.maxProjectiles) {
 					var projectile = Main.projectile[id];
-					var info = projectile.GetGlobalProjectile<ProjectileSourceItemInfo>();
 
-					info.UseTime = itemSource.Item.useTime;
-					info.UseAnimation = itemSource.Item.useAnimation;
-					info.Available = true;
+					if(source is ProjectileSource_Item itemSource) {
+						var info = projectile.GetGlobalProjectile<ProjectileSourceItemInfo>();
+
+						info.UseTime = itemSource.Item.useTime;
+						info.UseAnimation = itemSource.Item.useAnimation;
+						info.ManaUse = itemSource.Item.mana;
+						info.Available = true;
+					} else if(source is ProjectileSource_ProjectileParent parentSource) {
+						var parentInfo = parentSource.ParentProjectile.GetGlobalProjectile<ProjectileSourceItemInfo>();
+
+						if(parentInfo.Available) {
+							var info = projectile.GetGlobalProjectile<ProjectileSourceItemInfo>();
+
+							info.UseTime = parentInfo.UseTime;
+							info.UseAnimation = parentInfo.UseAnimation;
+							info.ManaUse = parentInfo.ManaUse;
+							info.Available = true;
+						}
+					}
 				}
 
 				return id;
