c7c120b3834782893b59c24a5bd4d92d2008f6b6
Mirsario <me@mirsar.io>
2021-04-10 23:27:06 +0300

    Improved low mana audio, added regen sound & dusts

--- Common/ModEntities/Players/PlayerManaSounds.cs
+++ Common/ModEntities/Players/PlayerManaSounds.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using ReLogic.Utilities;
 using Terraria;
 using Terraria.Audio;
+using Terraria.ID;
 using Terraria.ModLoader;
 using TerrariaOverhaul.Common.Systems.Time;
 using TerrariaOverhaul.Utilities;
@@ -13,7 +14,8 @@ namespace TerrariaOverhaul.Common.ModEntities.Players
 	[Autoload(Side = ModSide.Client)]
 	public class PlayerManaSounds : ModPlayer
 	{
-		public static readonly SoundStyle LowManaSound = new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Items/Magic/LowManaLoop", volume: 0.5f);
+		public static readonly SoundStyle ManaRegenSound = new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Items/Magic/ManaRegenLoop", volume: 0.03f);
+		public static readonly SoundStyle LowManaSound = new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Items/Magic/LowManaLoop", volume: 0.33f);
 		public static readonly Gradient<float> LowManaVolumeGradient = new Gradient<float>(
 			(0f, 1f),
 			(0.1f, 1f),
@@ -23,29 +25,93 @@ namespace TerrariaOverhaul.Common.ModEntities.Players
 		);
 
 		private SlotId lowManaSoundSlot;
-		private float lowManaSoundVolume;
+		private SlotId manaRegenSoundSlot;
+		private float lowManaEffectIntensity;
+		private float lowManaDustCounter;
+		private float manaRegenEffectIntensity;
+		private float manaRegenDustCounter;
 
 		public override void PreUpdate()
 		{
+			UpdateLowManaEffects();
+			UpdateManaRegenEffects();
+		}
+
+		private void UpdateLowManaEffects()
+		{
 			float manaFactor = Player.statMana / (float)Player.statManaMax2;
-			float goalLowManaVolume = LowManaVolumeGradient.GetValue(manaFactor);
+			float goalLowManaEffectIntensity = LowManaVolumeGradient.GetValue(manaFactor);
 
-			lowManaSoundVolume = MathUtils.StepTowards(lowManaSoundVolume, goalLowManaVolume, 3f * TimeSystem.LogicDeltaTime);
+			lowManaEffectIntensity = MathUtils.StepTowards(lowManaEffectIntensity, goalLowManaEffectIntensity, 0.75f * TimeSystem.LogicDeltaTime);
 
-			var sound = lowManaSoundSlot.IsValid ? SoundEngine.GetActiveSound(lowManaSoundSlot) : null;
+			//Sound
+			UpdateSound(ref lowManaSoundSlot, lowManaEffectIntensity, LowManaSound);
 
-			if(lowManaSoundVolume > 0f) {
-				if(sound == null) {
-					lowManaSoundSlot = SoundEngine.PlayTrackedSound(LowManaSound, Player.Center);
-					sound = SoundEngine.GetActiveSound(lowManaSoundSlot);
+			//Dust
+			lowManaDustCounter += lowManaEffectIntensity / 4f;
+
+			while(lowManaDustCounter >= 1f) {
+				var dust = Dust.NewDustDirect(Player.position, Player.width, Player.height, DustID.SomethingRed, Alpha: 255, Scale: Main.rand.NextFloat(1.5f, 2f));
+
+				dust.noLight = true;
+				dust.noGravity = true;
+				dust.velocity *= 0.25f;
+
+				lowManaDustCounter--;
+			}
+		}
+		private void UpdateManaRegenEffects()
+		{
+			float manaFactor = Player.statMana / (float)Player.statManaMax2;
+			float regenSpeed = Player.manaRegen + Player.manaRegenBonus;
+			float goalManaRegenEffectIntensity = manaFactor < 1f ? MathHelper.Clamp(regenSpeed / 30f, 0f, 1f) : 0f;
+
+			manaRegenEffectIntensity = MathUtils.StepTowards(manaRegenEffectIntensity, goalManaRegenEffectIntensity, 0.75f * TimeSystem.LogicDeltaTime);
+
+			//Sound
+			UpdateSound(ref manaRegenSoundSlot, manaRegenEffectIntensity, ManaRegenSound);
+
+			//Dust
+			manaRegenDustCounter += manaRegenEffectIntensity / 4f;
+
+			while(manaRegenDustCounter >= 1f) {
+				var dust = Dust.NewDustDirect(Player.position, Player.width, Player.height, 45, Alpha: 255, Scale: Main.rand.NextFloat(2f, 2.6f));
+
+				dust.noLight = true;
+				dust.noGravity = true;
+				dust.velocity *= 0.5f;
+
+				manaRegenDustCounter--;
+			}
+		}
+		private void UpdateSound(ref SlotId slot, float volume, SoundStyle style)
+		{
+			var sound = slot.IsValid ? SoundEngine.GetActiveSound(slot) : null;
+
+			if(volume > 0f) {
+				float styleVolume = style.Volume;
+
+				try {
+					if(sound == null) {
+						style.Volume = 0f;
+						slot = SoundEngine.PlayTrackedSound(style, Player.Center);
+						sound = SoundEngine.GetActiveSound(slot);
+
+						if(sound == null) {
+							return;
+						}
+					}
+
+					sound.Position = Player.Center;
+					sound.Volume = volume;
 				}
-
-				sound.Position = Player.Center;
-				sound.Volume = lowManaSoundVolume * 0.5f;
+				finally {
+					style.Volume = styleVolume;
+				}
 			} else if(sound != null) {
 				sound.Stop();
 
-				lowManaSoundSlot = SlotId.Invalid;
+				slot = SlotId.Invalid;
 			}
 		}
 	}
