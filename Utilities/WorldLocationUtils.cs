c24ced2fe82f9394f89942f7dfbfac7a13dbe6d1
Mirsario <me@mirsar.io>
2021-05-12 01:14:30 +0300

    Added WorldLocationUtils

---
+++ Utilities/WorldLocationUtils.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using TerrariaOverhaul.Utilities.DataStructures;
+
+namespace TerrariaOverhaul.Utilities
+{
+	public static class WorldLocationUtils
+	{
+		public static Gradient<float> SurfaceOrSkyGradient => new Gradient<float>(
+			(0f, 0f),
+			((float)Main.worldSurface * 0.2f, 1f),
+			((float)Main.worldSurface, 1f),
+			((float)Main.worldSurface * 1.05f, 0f)
+		);
+
+		public static Gradient<float> SurfaceGradient => new Gradient<float>(
+			((float)Main.worldSurface * 0.45f, 0f),
+			((float)Main.worldSurface, 1f),
+			((float)Main.worldSurface * 1.05f, 0f)
+		);
+
+		public static Gradient<float> UnderSurfaceGradient => new Gradient<float>(
+			((float)Main.worldSurface, 0f),
+			((float)Main.worldSurface * 1.1f, 1f)
+		);
+
+		public static Gradient<float> SpaceGradient => new Gradient<float>(
+			((float)Main.worldSurface * 0.15f, 1f),
+			((float)Main.worldSurface * 0.4f, 0f)
+		);
+	}
+}
