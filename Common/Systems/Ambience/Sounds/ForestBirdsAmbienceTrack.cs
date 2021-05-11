0f1a4ee4612a4cc0c5c1e2f918e33b5baf699ce5
Mirsario <me@mirsar.io>
2021-05-12 01:15:04 +0300

    Added two sounds to the forest.

---
+++ Common/Systems/Ambience/Sounds/ForestBirdsAmbienceTrack.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Common.Systems.Time;
+using TerrariaOverhaul.Utilities;
+
+namespace TerrariaOverhaul.Common.Systems.Ambience.Sounds
+{
+	public sealed class ForestBirdsAmbienceTrack : AmbienceTrack
+	{
+		public override void Initialize()
+		{
+			Sound = new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Atmosphere/Forest/ForestBirds", volume: 0.5f, type: SoundType.Ambient);
+		}
+		public override float GetTargetVolume(Player localPlayer)
+		{
+			if(!localPlayer.ZonePurity) {
+				return 0f;
+			}
+
+			float result = 1f;
+			
+			result *= TimeSystem.DayGradient.GetValue(TimeSystem.RealTime);
+			result *= WorldLocationUtils.SurfaceGradient.GetValue(localPlayer.Center.ToTileCoordinates().Y);
+
+			return result;
+		}
+	}
+}
---
+++ Common/Systems/Ambience/Sounds/ForestCricketsAmbienceTrack.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Common.Systems.Time;
+using TerrariaOverhaul.Utilities;
+
+namespace TerrariaOverhaul.Common.Systems.Ambience.Sounds
+{
+	public sealed class ForestCricketsAmbienceTrack : AmbienceTrack
+	{
+		public override void Initialize()
+		{
+			Sound = new ModSoundStyle(nameof(TerrariaOverhaul), "Assets/Sounds/Atmosphere/Forest/ForestCrickets", type: SoundType.Ambient);
+		}
+		public override float GetTargetVolume(Player localPlayer)
+		{
+			if(!localPlayer.ZonePurity) {
+				return 0f;
+			}
+
+			float result = 1f;
+
+			result *= TimeSystem.NightGradient.GetValue(TimeSystem.RealTime);
+			result *= WorldLocationUtils.SurfaceGradient.GetValue(localPlayer.Center.ToTileCoordinates().Y);
+
+			return result;
+		}
+	}
+}
