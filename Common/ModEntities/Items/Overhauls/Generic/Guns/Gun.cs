11470a4b146cbff464326ddcde414ee612d60182
Mirsario <me@mirsar.io>
2021-05-12 22:17:25 +0300

    Experimental: Gun recoil.

--- Common/ModEntities/Items/Overhauls/Generic/Guns/Gun.cs
+++ Common/ModEntities/Items/Overhauls/Generic/Guns/Gun.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using TerrariaOverhaul.Common.Hooks.Items;
+using TerrariaOverhaul.Common.Systems.CursorOffsets;
+using TerrariaOverhaul.Utilities.Extensions;
 
 namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls.Generic.Guns
 {
 	public abstract class Gun : AdvancedItem, IShowItemCrosshair
 	{
 		public int MuzzleflashTime { get; protected set; }
+
+		public virtual float GetRecoilStrength(Item item, Player player)
+		{
+			float baseRecoil = Math.Min(item.useTime * 0.175f, 3f);
+			float randomizedRecoil = baseRecoil * Main.rand.NextFloat(0.75f, 1.333f);
+
+			return randomizedRecoil;
+		}
 
 		public override void HoldItem(Item item, Player player)
 		{
@@ -19,7 +30,11 @@ namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls.Generic.Guns
 		}
 		public override bool? UseItem(Item item, Player player)
 		{
-			MuzzleflashTime = System.Math.Max(MuzzleflashTime, 5);
+			MuzzleflashTime = Math.Max(MuzzleflashTime, 5);
+
+			if(!Main.dedServ && player.IsLocal()) {
+				ApplyRecoil(item, player);
+			}
 
 			return base.UseItem(item, player);
 		}
@@ -36,5 +51,23 @@ namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls.Generic.Guns
 		}
 
 		public bool ShowItemCrosshair(Item item, Player player) => true;
+
+		protected void ApplyRecoil(Item item, Player player)
+		{
+			float strength = GetRecoilStrength(item, player);
+
+			if(strength <= 0f) {
+				return;
+			}
+
+			var mousePos = new Vector2(Main.mouseX, Main.mouseY);
+			var origin = player.Center - Main.screenPosition;
+
+			strength *= 1f - MathHelper.Clamp(Vector2.Distance(mousePos, origin) / Main.screenWidth, 0f, 1f);
+
+			var offset = mousePos.RotatedBy(MathHelper.ToRadians(player.direction * -strength), origin) - mousePos;
+
+			CursorOffsetSystem.AddCursorOffset(offset, 20f);
+		}
 	}
 }
---
+++ Common/Systems/CursorOffsets/CursorOffsetSystem.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Terraria;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Utilities;
+using TerrariaOverhaul.Utilities.DataStructures;
+
+namespace TerrariaOverhaul.Common.Systems.CursorOffsets
+{
+	[Autoload(Side = ModSide.Client)]
+	public sealed class CursorOffsetSystem : ModSystem
+	{
+		private struct CursorOffset
+		{
+			public float speed;
+			public Vector2 remainder;
+			public Vector2 counter;
+
+			public CursorOffset(Vector2 length, float speed)
+			{
+				this.speed = speed;
+				remainder = length;
+				counter = default;
+			}
+		}
+
+		private static List<CursorOffset> offsets;
+
+		public override void Load()
+		{
+			offsets = new List<CursorOffset>();
+
+			Main.OnPostDraw += PostDraw;
+		}
+		public override void Unload()
+		{
+			offsets = null;
+
+			Main.OnPostDraw -= PostDraw;
+		}
+
+		private void PostDraw(GameTime gameTime)
+		{
+			var mouseState = Mouse.GetState();
+			var mousePos = new Vector2Int(mouseState.X, mouseState.Y);
+			var newMousePos = mousePos;
+
+			for(int i = 0; i < offsets.Count; i++) {
+				var info = offsets[i];
+				var remaining = info.remainder;
+				var newRemainder = remaining * (1f - (float)gameTime.ElapsedGameTime.TotalSeconds * info.speed);
+
+				const float Threshold = 0.01f;
+
+				if(Math.Abs(newRemainder.X) < Threshold) {
+					newRemainder.X = 0f;
+				}
+
+				if(Math.Abs(newRemainder.Y) < Threshold) {
+					newRemainder.Y = 0f;
+				}
+
+				info.counter += remaining - newRemainder;
+				info.remainder = newRemainder;
+
+				var flooredGain = (Vector2Int)Vector2Utils.Floor(info.counter);
+
+				newMousePos += flooredGain;
+				info.counter -= flooredGain;
+
+				if(info.remainder == default) {
+					offsets.RemoveAt(i--);
+				} else {
+					offsets[i] = info;
+				}
+			}
+
+			if(mousePos != newMousePos) {
+				Mouse.SetPosition(newMousePos.X, newMousePos.Y);
+
+				Main.mouseX = newMousePos.X;
+				Main.mouseY = newMousePos.Y;
+			}
+		}
+
+		public static void AddCursorOffset(Vector2 offset, float speed)
+		{
+			offsets.Add(new CursorOffset(offset, speed));
+		}
+	}
+}
