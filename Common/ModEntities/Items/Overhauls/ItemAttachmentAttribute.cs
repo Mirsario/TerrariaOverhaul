e815465827093f324d2542182b4176943d5a5c18
Mirsario <me@mirsar.io>
2021-05-12 23:55:04 +0300

    Implemented ItemAttachmentAttribute, for forcing items to use item overhauls.

---
+++ Common/ModEntities/Items/Overhauls/ItemAttachmentAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public sealed class ItemAttachmentAttribute : Attribute
+	{
+		public readonly IReadOnlyList<int> ItemIds;
+
+		public ItemAttachmentAttribute(params int[] itemIds)
+		{
+			ItemIds = Array.AsReadOnly(itemIds);
+		}
+	}
+}
--- Common/ModEntities/Items/Overhauls/ItemOverhaul.cs
+++ Common/ModEntities/Items/Overhauls/ItemOverhaul.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -6,7 +9,8 @@ namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls
 {
 	public abstract class ItemOverhaul : GlobalItem
 	{
-		private static List<ItemOverhaul> itemOverhauls;
+		private static List<ItemOverhaul> itemOverhauls = new();
+		private static Dictionary<int, int> itemIdMapping = new();
 
 		protected Item item;
 
@@ -18,19 +22,21 @@ namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls
 		//
 		public override void Load()
 		{
-			if(itemOverhauls == null) {
-				itemOverhauls = new List<ItemOverhaul>();
+			int id = itemOverhauls.Count;
+			var attachments = GetType().GetCustomAttribute<ItemAttachmentAttribute>();
+
+			if(attachments != null) {
+				foreach(int itemId in attachments.ItemIds) {
+					itemIdMapping[itemId] = id;
+				}
 			}
 
 			itemOverhauls.Add(this);
 		}
 		public override void Unload()
 		{
-			if(itemOverhauls != null) {
-				itemOverhauls.Clear();
-
-				itemOverhauls = null;
-			}
+			itemOverhauls.Clear();
+			itemIdMapping.Clear();
 		}
 		public override GlobalItem Clone(Item item, Item itemClone)
 		{
@@ -43,6 +49,10 @@ namespace TerrariaOverhaul.Common.ModEntities.Items.Overhauls
 
 		public static ItemOverhaul ChooseItemOverhaul(Item item)
 		{
+			if(itemIdMapping.TryGetValue(item.type, out int overhaulId)) {
+				return itemOverhauls[overhaulId];
+			}
+
 			//May need some sort of priority system in the future. And cache?
 			for(int i = 0; i < itemOverhauls.Count; i++) {
 				var itemOverhaul = itemOverhauls[i];
