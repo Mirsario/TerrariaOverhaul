80362da838ae4e47b02f12e624a2a19e57ea662d
Mirsario <me@mirsar.io>
2021-05-16 21:13:04 +0300

    Vanilla snowing for winter.

--- Common/Systems/Seasons/Winter.cs
+++ Common/Systems/Seasons/Winter.cs
@@ -4,6 +4,9 @@ namespace TerrariaOverhaul.Common.Systems.Seasons
 {
 	public class Winter : Season
 	{
-		
+		protected internal override void Init()
+		{
+			Components.Add(new SnowSeasonComponent());
+		}
 	}
 }
