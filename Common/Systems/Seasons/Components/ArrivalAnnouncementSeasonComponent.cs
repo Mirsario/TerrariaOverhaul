21ee60e49364defc377bc244e5ade6a521ec9f76
Mirsario <me@mirsar.io>
2021-05-16 21:21:05 +0300

    Basic season arrival announcement.

---
+++ Common/Systems/Seasons/Components/ArrivalAnnouncementSeasonComponent.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using TerrariaOverhaul.Core.Components;
+
+namespace TerrariaOverhaul.Common.Systems.Seasons.Components
+{
+	[GlobalComponent]
+	public sealed class ArrivalAnnouncementSeasonComponent : SeasonComponent
+	{
+		public override void OnSeasonBegin(Season season)
+		{
+			Main.NewText($"Season {season.Name} is here.");
+		}
+	}
+}
