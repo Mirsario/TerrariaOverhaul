7eae18128f05e897d86065d909bfe6fed4a1d391
Mirsario <me@mirsar.io>
2021-08-24 02:37:59 +0300

    CommonStatMultipliers utility struct and hooks.

---
+++ Common/Hooks/Items/IModifyCommonStatMultipliers.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
+using TerrariaOverhaul.Utilities.DataStructures;
+using Hook = TerrariaOverhaul.Common.Hooks.Items.IModifyCommonStatMultipliers;
+
+namespace TerrariaOverhaul.Common.Hooks.Items
+{
+	public interface IModifyCommonStatMultipliers
+	{
+		public delegate void Delegate(Item item, Player player, ref CommonStatMultipliers multipliers);
+
+		public static readonly HookList<GlobalItem, Delegate> Hook = ItemLoader.AddModHook(new HookList<GlobalItem, Delegate>(
+			//Method reference
+			typeof(Hook).GetMethod(nameof(ModifyCommonStatMultipliers)),
+			//Invocation
+			e => (Item item, Player player, ref CommonStatMultipliers multipliers) => {
+				(item.ModItem as Hook)?.ModifyCommonStatMultipliers(item, player, ref multipliers);
+
+				foreach(Hook g in e.Enumerate(item)) {
+					g.ModifyCommonStatMultipliers(item, player, ref multipliers);
+				}
+			}
+		));
+
+		void ModifyCommonStatMultipliers(Item item, Player player, ref CommonStatMultipliers multipliers);
+
+		public static CommonStatMultipliers GetMultipliers(Item item, Player player)
+		{
+			var multipliers = CommonStatMultipliers.Default;
+
+			Hook.Invoke(item, player, ref multipliers);
+
+			return multipliers;
+		}
+	}
+
+	public sealed class ItemModifyCommonStatMultipliersImplementation : GlobalItem
+	{
+		public override void ModifyHitNPC(Item item, Player player, NPC target, ref int damage, ref float knockback, ref bool crit)
+		{
+			var multipliers = Hook.GetMultipliers(item, player);
+
+			damage = (int)(damage * multipliers.MeleeDamageMultiplier);
+			knockback *= multipliers.MeleeKnockbackMultiplier;
+		}
+
+		public override void ModifyShootStats(Item item, Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+			var multipliers = Hook.GetMultipliers(item, player);
+
+			damage = (int)(damage * multipliers.ProjectileDamageMultiplier);
+			knockback *= multipliers.ProjectileKnockbackMultiplier;
+			velocity *= multipliers.ProjectileSpeedMultiplier;
+		}
+
+		public void ModifyMeleeRange(Item item, Player player, ref float range)
+		{
+			var multipliers = Hook.GetMultipliers(item, player);
+
+			range *= multipliers.MeleeRangeMultiplier;
+		}
+	}
+}
---
+++ Utilities/DataStructures/CommonStatMultipliers.cs
@@ -0,0 +1,33 @@
+﻿namespace TerrariaOverhaul.Utilities.DataStructures
+{
+	public struct CommonStatMultipliers
+	{
+		public static readonly CommonStatMultipliers Default = new() {
+			MeleeDamageMultiplier = 1f,
+			MeleeKnockbackMultiplier = 1f,
+			MeleeRangeMultiplier = 1f,
+			ProjectileDamageMultiplier = 1f,
+			ProjectileKnockbackMultiplier = 1f,
+			ProjectileSpeedMultiplier = 1f,
+		};
+
+		public float MeleeDamageMultiplier;
+		public float MeleeKnockbackMultiplier;
+		public float MeleeRangeMultiplier;
+		public float ProjectileDamageMultiplier;
+		public float ProjectileKnockbackMultiplier;
+		public float ProjectileSpeedMultiplier;
+
+		public static CommonStatMultipliers operator *(CommonStatMultipliers a, CommonStatMultipliers b)
+		{
+			a.MeleeDamageMultiplier *= b.MeleeDamageMultiplier;
+			a.MeleeKnockbackMultiplier *= b.MeleeKnockbackMultiplier;
+			a.MeleeRangeMultiplier *= b.MeleeRangeMultiplier;
+			a.ProjectileDamageMultiplier *= b.ProjectileDamageMultiplier;
+			a.ProjectileKnockbackMultiplier *= b.ProjectileKnockbackMultiplier;
+			a.ProjectileSpeedMultiplier *= b.ProjectileSpeedMultiplier;
+
+			return a;
+		}
+	}
+}
