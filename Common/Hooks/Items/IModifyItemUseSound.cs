f8fd58e17d4f0f374be55ef1d9e45d81bb356b50
Mirsario <me@mirsar.io>
2022-01-14 20:36:23 +0300

    Basic ModifyItemUseSound hook

---
+++ Common/Hooks/Items/IModifyItemUseSound.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
+using Hook = TerrariaOverhaul.Common.Hooks.Items.IModifyItemUseSound;
+
+namespace TerrariaOverhaul.Common.Hooks.Items
+{
+	public interface IModifyItemUseSound
+	{
+		public delegate void Delegate(Item item, Player player, ref ISoundStyle useSound);
+
+		public static readonly HookList<GlobalItem, Delegate> Hook = ItemLoader.AddModHook(new HookList<GlobalItem, Delegate>(
+			// Method reference
+			typeof(Hook).GetMethod(nameof(ModifyItemUseSound)),
+			// Invocation
+			e => (Item item, Player player, ref ISoundStyle useSound) => {
+				(item.ModItem as Hook)?.ModifyItemUseSound(item, player, ref useSound);
+
+				foreach (Hook g in e.Enumerate(item)) {
+					g.ModifyItemUseSound(item, player, ref useSound);
+				}
+			}
+		));
+
+		void ModifyItemUseSound(Item item, Player player, ref ISoundStyle useSound);
+	}
+}
