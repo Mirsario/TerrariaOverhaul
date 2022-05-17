5f77a6daf9d0d9757fc9101631ae51374cc05996
Mirsario <me@mirsar.io>
2022-05-17 13:39:02 +0300

    ItemUtils.NewItemInstanced

--- Utilities/ItemUtils.cs
+++ Utilities/ItemUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
@@ -8,6 +9,25 @@ namespace TerrariaOverhaul.Utilities
 {
 	public static class ItemUtils
 	{
-		
+		public static void NewItemInstanced(IEntitySource source, Vector2 position, int type, int stack = 1, IEnumerable<Player>? players = null, int maxExpectedLifeTime = 54000, int prefix = 0)
+		{
+			if (Main.netMode == NetmodeID.MultiplayerClient) {
+				throw new InvalidOperationException($"{nameof(NewItemInstanced)} must not be called on multiplayer clients.");
+			}
+
+			int itemId = Item.NewItem(source, position, type, stack, true, prefix);
+
+			if (Main.netMode == NetmodeID.Server) {
+				players ??= ActiveEntities.Players;
+
+				Main.timeItemSlotCannotBeReusedFor[itemId] = maxExpectedLifeTime;
+
+				foreach (var player in players) {
+					NetMessage.SendData(MessageID.InstancedItem, player.whoAmI, -1, null, itemId);
+				}
+
+				Main.item[itemId].active = false;
+			}
+		}
 	}
 }
