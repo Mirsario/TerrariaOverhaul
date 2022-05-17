3cea920fe4ef78f791d41c67836f2c6856d8c02b
Mirsario <me@mirsar.io>
2022-05-17 13:37:29 +0300

    Removed unused util.

--- Utilities/ItemUtils.cs
+++ Utilities/ItemUtils.cs
@@ -1,25 +1,13 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 
 namespace TerrariaOverhaul.Utilities
 {
 	public static class ItemUtils
 	{
-		public static float GetHeavyness(Item item)
-		{
-			const float HeaviestSpeed = 0.5f;
-			const float LightestSpeed = 5f;
-
-			float speed = 1f / (Math.Max(1, item.useAnimation) / 60f);
-			float speedResult = MathHelper.Clamp(MathUtils.InverseLerp(speed, LightestSpeed, HeaviestSpeed), 0f, 1f);
-
-			float averageDimension = (item.width + item.height) * 0.5f;
-			float sizeResult = Math.Max(0f, averageDimension / 10f);
-
-			float result = speedResult * sizeResult;
-
-			return MathHelper.Clamp(result, 0f, 1f);
-		}
+		
 	}
 }
