73a22b640c707fca569bd53a3cc1ca4642db857e
Mirsario <me@mirsar.io>
2022-06-01 06:04:12 +0300

    Monster Banner Rework Re-added

---
+++ Common/Banners/BannerReworkSystem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Core.Configuration;
+using TerrariaOverhaul.Utilities;
+
+namespace TerrariaOverhaul.Common.Banners
+{
+	public sealed class BannerReworkSystem : ModSystem
+	{
+		private static bool doubledLoot;
+		private static bool bannerDamageDisabled;
+		private static ItemID.BannerEffect[]? defaultBannerEffects;
+
+		public static readonly ConfigEntry<bool> BannerReworkEnabled = new(ConfigSide.Both, "Banners", nameof(BannerReworkEnabled), () => true);
+
+		public override void Load()
+		{
+			On.Terraria.NPC.NPCLoot_DropItems += (orig, npc, closestPlayer) => {
+				orig(npc, closestPlayer);
+
+				if (ShouldDoubleLoot(npc)) {
+					orig(npc, closestPlayer);
+				}
+			};
+			
+			On.Terraria.NPC.NPCLoot_DropMoney += (orig, npc, closestPlayer) => {
+				orig(npc, closestPlayer);
+
+				if (ShouldDoubleLoot(npc)) {
+					orig(npc, closestPlayer);
+				}
+			};
+		}
+
+		public override void PreUpdateWorld()
+		{
+			bool enable = BannerReworkEnabled.Value;
+
+			if (enable != bannerDamageDisabled) {
+				if (enable) {
+					if (defaultBannerEffects == null) {
+						defaultBannerEffects = ItemID.Sets.BannerStrength;
+					}
+
+					ItemID.Sets.BannerStrength = ItemID.Sets.Factory.CreateCustomSet(new ItemID.BannerEffect(1f, 1f, 1f, 1f));
+				} else {
+					ItemID.Sets.BannerStrength = defaultBannerEffects ?? throw new Exception($"{nameof(BannerReworkSystem)} failed miserably!");
+				}
+
+				bannerDamageDisabled = enable;
+			}
+		}
+
+		private static bool ShouldDoubleLoot(NPC npc)
+		{
+			if (npc.type < NPCID.None || !BannerReworkEnabled) {
+				return false;
+			}
+			
+			int bannerId = Item.NPCtoBanner(npc.BannerID());
+
+			if (bannerId <= 0) {
+				return false;
+			}
+			
+			const float MinDistanceSquared = 2048f * 2048f;
+
+			Vector2 npcCenter = npc.Center;
+
+			foreach (var player in ActiveEntities.Players) {
+				if (player.HasNPCBannerBuff(bannerId) && Vector2.DistanceSquared(player.Center, npcCenter) < MinDistanceSquared) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Common/Banners/BuffBannerRework.cs
@@ -0,0 +1,16 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Utilities;
+
+namespace TerrariaOverhaul.Common.Banners
+{
+	public sealed class BuffBannerRework : GlobalBuff
+	{
+		public override void ModifyBuffTip(int type, ref string tip, ref int rare)
+		{
+			if (BannerReworkSystem.BannerReworkEnabled && type == BuffID.MonsterBanner) {
+				tip = OverhaulMod.Instance.GetTextValue("Banners.BannerBuffDescription");
+			}
+		}
+	}
+}
