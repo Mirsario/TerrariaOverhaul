bdd622de2ff545595a0c39445ca858afd9cfd707
Mirsario <me@mirsar.io>
2022-06-05 23:21:03 +0300

    Added 'Accessibility.ForceAutoReuse' config option.

--- Common/Accessibility/ItemAutoReuse.cs
+++ Common/Accessibility/ItemAutoReuse.cs
@@ -1,17 +1,26 @@
 ﻿using Terraria;
 using Terraria.ModLoader;
+using TerrariaOverhaul.Core.Configuration;
 
 namespace TerrariaOverhaul.Common.Accessibility
 {
 	public class ItemAutoReuse : GlobalItem
 	{
+		public static readonly ConfigEntry<bool> ForceAutoReuse = new(ConfigSide.ClientOnly, "Accessibility", nameof(ForceAutoReuse), () => true);
+
 		public override void SetDefaults(Item item)
 		{
-			if (!item.autoReuse && !item.channel) {
-				item.autoReuse = true;
-				item.useTime += 2;
-				item.useAnimation += 2;
+			if (!ForceAutoReuse) {
+				return;
 			}
+
+			if (item.autoReuse || item.channel) {
+				return;
+			}
+
+			item.autoReuse = true;
+			item.useTime += 2;
+			item.useAnimation += 2;
 		}
 	}
 }
