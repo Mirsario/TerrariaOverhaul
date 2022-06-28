52f50fc4f4dc5c0e41c96c2dbbf234257645e212
Jakson <77369015+JaksonD@users.noreply.github.com>
2022-06-27 19:38:51 -0500

    Take into account weapon scale when calculating melee range (#116)
    * Update ItemMeleeAttackAiming.cs
    
    woop
    
    * Base range on approximate melee hitboxes instead.
    
    * Invoke UseItemHitbox hook.
    
    Co-authored-by: Mirsario <me@mirsar.io>

--- Common/Melee/ItemMeleeAttackAiming.cs
+++ Common/Melee/ItemMeleeAttackAiming.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 using TerrariaOverhaul.Common.Hooks.Items;
 using TerrariaOverhaul.Common.ModEntities.NPCs;
+using TerrariaOverhaul.Core.Debugging;
 using TerrariaOverhaul.Core.ItemComponents;
 using TerrariaOverhaul.Utilities;
 
@@ -43,25 +47,119 @@ namespace TerrariaOverhaul.Common.Melee
 			}
 		}
 
-		public bool? CanMeleeCollideWithNPC(Item item, Player player, NPC target)
+		public bool? CanMeleeCollideWithNPC(Item item, Player player, NPC target, Rectangle itemRectangle)
 		{
 			if (!Enabled) {
 				return null;
 			}
 
-			float range = GetAttackRange(item, player);
+			float range = GetAttackRange(item, player, itemRectangle);
+
+			if (!Main.dedServ && DebugSystem.EnableDebugRendering && itemRectangle) {
+				DebugSystem.DrawRectangle(itemRectangle, Color.Purple);
+			}
 
 			// Check arc collision
 			return CollisionUtils.CheckRectangleVsArcCollision(target.getRect(), player.Center, AttackAngle, MathHelper.Pi * 0.5f, range);
 		}
 
-		public static float GetAttackRange(Item item, Player player)
+		public static float GetAttackRange(Item item, Player player, Rectangle? itemRectangle = null)
 		{
-			float range = (item.Size * item.scale * 1.25f).Length();
+			Rectangle itemHitbox = GetMeleeHitbox(player, item);
 
-			IModifyItemMeleeRange.Invoke(item, player, ref range);
+			if (!Main.dedServ && DebugSystem.EnableDebugRendering) {
+				DebugSystem.DrawRectangle(itemHitbox, Color.LightGoldenrodYellow);
+			}
+
+			float range = 0f;
+			var playerCenter = player.Center;
+			var hitboxCenter = itemHitbox.Center;
+
+			for (int i = 0; i < 4; i++) {
+				var corner = i switch {
+					0 => itemHitbox.TopLeft(),
+					1 => itemHitbox.TopRight(),
+					2 => itemHitbox.BottomRight(),
+					3 => itemHitbox.BottomLeft(),
+					_ => throw new InvalidOperationException()
+				};
+				float distanceToCorner = Vector2.Distance(playerCenter, corner);
+
+				range = Math.Max(range, distanceToCorner);
+			}
+
+			//IModifyItemMeleeRange.Invoke(item, player, ref range);
 
 			return range;
+		}
+
+		public static Rectangle GetMeleeHitbox(Player player, Item item)
+		{
+			// Partially based on Player.ItemCheck_GetMeleeHitbox().
+			// Would've been best to somehow make a shortened version of it via injections, but whatever.
+
+			var playerCenter = player.Top;
+			var itemRectangle = new Rectangle((int)playerCenter.X, (int)playerCenter.Y, 32, 32);
+
+			if (!Main.dedServ) {
+				Rectangle drawHitbox = Item.GetDrawHitbox(item.type, player);
+				var size = drawHitbox.Size() + item.type switch {
+					// Vanilla-derived hardcode.
+					5094 or 5095 => new(-10, -10),
+					5096 => new(-12, -12),
+					5097 => new(-8, -8),
+					_ => default
+				};
+
+				itemRectangle.Width = (int)size.X;
+				itemRectangle.Height = (int)size.Y;
+			}
+			
+			float adjustedItemScale = player.GetAdjustedItemScale(item);
+			
+			itemRectangle.Width = (int)(itemRectangle.Width * adjustedItemScale);
+			itemRectangle.Height = (int)(itemRectangle.Height * adjustedItemScale);
+
+			if (player.direction == -1) {
+				itemRectangle.X -= itemRectangle.Width;
+			}
+
+			if (player.gravDir == 1f) {
+				itemRectangle.Y -= itemRectangle.Height;
+			}
+
+			if (item.useStyle == ItemUseStyleID.Swing) {
+				if (player.direction == -1) {
+					itemRectangle.X -= (int)(itemRectangle.Width * 1.4 - itemRectangle.Width);
+				}
+
+				itemRectangle.Width = (int)(itemRectangle.Width * 1.4);
+				itemRectangle.Y += (int)(itemRectangle.Height * 0.5 * (double)player.gravDir);
+				itemRectangle.Height = (int)(itemRectangle.Height * 1.1);
+			} else if (item.useStyle == ItemUseStyleID.Thrust) {
+				if (player.direction == -1) {
+					itemRectangle.X -= (int)(itemRectangle.Width * 1.4 - itemRectangle.Width);
+				}
+
+				itemRectangle.Width = (int)(itemRectangle.Width * 1.4);
+				itemRectangle.Y += (int)(itemRectangle.Height * 0.6);
+				itemRectangle.Height = (int)(itemRectangle.Height * 0.6);
+				
+				if (item.type == ItemID.Umbrella || item.type == ItemID.TragicUmbrella) {
+					itemRectangle.Height += 14;
+					itemRectangle.Width -= 10;
+
+					if (player.direction == -1) {
+						itemRectangle.X += 10;
+					}
+				}
+			}
+
+			bool dontAttackDummy = false;
+
+			ItemLoader.UseItemHitbox(item, player, ref itemRectangle, ref dontAttackDummy);
+
+			return itemRectangle;
 		}
 	}
 }
