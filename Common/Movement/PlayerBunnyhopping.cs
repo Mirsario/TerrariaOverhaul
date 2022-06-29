56ec160c846e8a64fae4efe7d2a7cd0891f86e31
Mirsario <me@mirsar.io>
2022-06-30 02:50:22 +0300

    Added "Bunnyroll" combos.

--- Common/Movement/PlayerBunnyhopping.cs
+++ Common/Movement/PlayerBunnyhopping.cs
@@ -1,4 +1,9 @@
-﻿using Terraria.ModLoader;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Common.Dodgerolls;
 using TerrariaOverhaul.Core.Configuration;
 using TerrariaOverhaul.Utilities;
 
@@ -8,7 +13,7 @@ namespace TerrariaOverhaul.Common.Movement
 	{
 		public static readonly ConfigEntry<bool> EnableBunnyhopping = new(ConfigSide.Both, "PlayerMovement", nameof(EnableBunnyhopping), () => true);
 
-		public static float DefaultBoost => 0.8f;
+		public static float DefaultBoost => 0.825f;
 
 		public float Boost { get; set; }
 
@@ -27,7 +32,27 @@ namespace TerrariaOverhaul.Common.Movement
 			bool wasOnGround = Player.WasOnGround();
 
 			if (!onGround && wasOnGround && Player.velocity.Y < 0f) {
-				Player.velocity.X += Boost * Player.KeyDirection();
+				float boost = Boost;
+
+				if (Player.TryGetModPlayer(out PlayerDodgerolls dodgerolls) && dodgerolls.IsDodging) {
+					boost += 0.5f;
+
+					if (!Main.dedServ) {
+						var playerCenter = Player.Center;
+						var entitySource = Player.GetSource_FromThis();
+
+						for (int i = 0; i < 3; i++) {
+							var position = playerCenter + new Vector2(Main.rand.NextFloat(-4f, 4f), 0f);
+							var velocity = new Vector2((i - 1) * 0.9f, -0.1f);
+
+							Gore.NewGorePerfect(entitySource, position, velocity, GoreID.Smoke1 + i);
+						}
+
+						SoundEngine.PlaySound(SoundID.DoubleJump, playerCenter);
+					}
+				}
+
+				Player.velocity.X += boost * Player.KeyDirection();
 			}
 		}
 	}
