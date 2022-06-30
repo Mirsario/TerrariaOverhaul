890071084c1dcce777afa7b903b7a84ce55d4d99
Mirsario <me@mirsar.io>
2022-06-30 03:02:23 +0300

    Useless and commented-out IPlayerOnBunnyhopHook.

---
+++ Common/Movement/_Hooks/IPlayerOnBunnyhopHook.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
+/*
+using Hook = TerrariaOverhaul.Common.Movement.IPlayerOnBunnyhopHook;
+
+namespace TerrariaOverhaul.Common.Movement
+{
+	public interface IPlayerOnBunnyhopHook
+	{
+		public static readonly HookList<ModPlayer> Hook = PlayerLoader.AddModHook(new HookList<ModPlayer>(typeof(Hook).GetMethod(nameof(OnBunnyhop))));
+
+		void OnBunnyhop(Player player, ref float boost);
+
+		public static void Invoke(Player player, ref float boost)
+		{
+			foreach (Hook g in Hook.Enumerate(player)) {
+				g.OnBunnyhop(player, ref boost);
+			}
+		}
+	}
+}
+*/
