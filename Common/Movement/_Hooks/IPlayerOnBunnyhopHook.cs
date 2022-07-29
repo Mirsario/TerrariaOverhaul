6ff0e47338f0d7c6fe75e3990d03bb1c8f0e395b
Mirsario <me@mirsar.io>
2022-07-29 08:27:32 +0300

    Hook repaired?

--- Common/Movement/_Hooks/IPlayerOnBunnyhopHook.cs
+++ Common/Movement/_Hooks/IPlayerOnBunnyhopHook.cs
@@ -1,23 +1,20 @@
 ﻿using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Core;
-/*
 using Hook = TerrariaOverhaul.Common.Movement.IPlayerOnBunnyhopHook;
 
-namespace TerrariaOverhaul.Common.Movement
+namespace TerrariaOverhaul.Common.Movement;
+
+public interface IPlayerOnBunnyhopHook
 {
-	public interface IPlayerOnBunnyhopHook
+	public static readonly HookList<ModPlayer> Hook = PlayerLoader.AddModHook(new HookList<ModPlayer>(typeof(Hook).GetMethod(nameof(OnBunnyhop))));
+
+	void OnBunnyhop(Player player, ref float boost);
+
+	public static void Invoke(Player player, ref float boost)
 	{
-		public static readonly HookList<ModPlayer> Hook = PlayerLoader.AddModHook(new HookList<ModPlayer>(typeof(Hook).GetMethod(nameof(OnBunnyhop))));
-
-		void OnBunnyhop(Player player, ref float boost);
-
-		public static void Invoke(Player player, ref float boost)
-		{
-			foreach (Hook g in Hook.Enumerate(player)) {
-				g.OnBunnyhop(player, ref boost);
-			}
+		foreach (Hook g in Hook.Enumerate(player.ModPlayers)) {
+			g.OnBunnyhop(player, ref boost);
 		}
 	}
 }
-*/
