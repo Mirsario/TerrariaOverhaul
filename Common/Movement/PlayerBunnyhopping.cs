d4298a3dc0dd256dcea61c8e3208c41641b26181
Mirsario <me@mirsar.io>
2022-07-29 11:52:54 +0300

    Bunnyhop + Dodgeroll combos. (#139)
    * Added "Bunnyroll" combos.
    
    * Useless and commented-out IPlayerOnBunnyhopHook.
    
    * Hook repaired?
    
    * Updated implementation.
    
    * Added a bunnyroll sound.
    
    * Bunnyhops now only happen on landing.
    
    * Changed audio.
    
    * Changed effect to x1.5 boost multiplication.

--- Common/Movement/PlayerBunnyhopping.cs
+++ Common/Movement/PlayerBunnyhopping.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ModLoader;
 using TerrariaOverhaul.Core.Configuration;
 using TerrariaOverhaul.Utilities;
 
@@ -10,6 +11,7 @@ public sealed class PlayerBunnyhopping : ModPlayer
 
 	public static float DefaultBoost => 0.8f;
 
+	public uint NumTicksOnGround { get; set; }
 	public float Boost { get; set; }
 
 	public override void ResetEffects()
@@ -17,17 +19,32 @@ public sealed class PlayerBunnyhopping : ModPlayer
 		Boost = DefaultBoost;
 	}
 
-	public override void PostItemCheck()
+	public override bool PreItemCheck()
 	{
 		if (!EnableBunnyhopping) {
-			return;
+			return base.PreItemCheck();
 		}
 
 		bool onGround = Player.OnGround();
 		bool wasOnGround = Player.WasOnGround();
+		
+		if (!onGround && wasOnGround && NumTicksOnGround < 3) {
+			float boostAdd = 0f;
+			float boostMultiplier = 1f;
 
-		if (!onGround && wasOnGround && Player.velocity.Y < 0f) {
-			Player.velocity.X += Boost * Player.KeyDirection().X;
+			IPlayerOnBunnyhopHook.Invoke(Player, ref boostAdd, ref boostMultiplier);
+
+			float totalBoost = (Boost + boostAdd) * boostMultiplier;
+
+			Player.velocity.X += Player.KeyDirection().X * totalBoost;
 		}
+
+		if (onGround) {
+			NumTicksOnGround++;
+		} else {
+			NumTicksOnGround = 0;
+		}
+
+		return base.PreItemCheck();
 	}
 }
