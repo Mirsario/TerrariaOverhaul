666403887637355a87758de4f4efe10e6a2934ac
Mirsario <me@mirsar.io>
2022-08-03 12:41:34 +0300

    Fixed #118 - Driver & blood related world texture corruption.

---
+++ Utilities/_Extensions/Texture2DExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TerrariaOverhaul.Utilities;
+
+internal static class Texture2DExtensions
+{
+	public static void InitializeWithColor(this Texture2D texture, Color color)
+	{
+		var data = new Color[texture.Width * texture.Height];
+
+		Array.Fill(data, color);
+
+		texture.SetData(data);
+	}
+}
