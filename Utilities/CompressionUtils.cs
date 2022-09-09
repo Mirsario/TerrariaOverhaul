8c0ddf0d6e6fc9ec8777fa2d40b9cdcbee479a95
Mirsario <me@mirsar.io>
2022-09-09 20:10:51 +0300

    CompressionUtils

---
+++ Utilities/CompressionUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace TerrariaOverhaul.Utilities;
+
+internal static class CompressionUtils
+{
+	public static byte[] DeflateCompress(byte[] data, CompressionLevel compressionLevel = CompressionLevel.Optimal)
+	{
+		using var outputStream = new MemoryStream();
+		using var deflateStream = new DeflateStream(outputStream, compressionLevel, leaveOpen: true);
+		
+		deflateStream.Write(data);
+		deflateStream.Flush();
+
+		return outputStream.ToArray();
+	}
+
+	public static byte[] DeflateDecompress(byte[] data)
+	{
+		using var inputStream = new MemoryStream(data);
+		using var outputStream = new MemoryStream();
+		using var deflateStream = new DeflateStream(inputStream, CompressionMode.Decompress, leaveOpen: true);
+
+		deflateStream.CopyTo(outputStream);
+
+		return outputStream.ToArray();
+	}
+}
