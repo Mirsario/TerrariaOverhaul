63733bc72a393ac2cb09fa9877e6ab058b082757
Mirsario <me@mirsar.io>
2022-09-11 18:06:37 +0300

    Fixed #149 (Config button may crash the game)

--- Common/MainMenuOverlays/ConfigurationMenuButton.cs
+++ Common/MainMenuOverlays/ConfigurationMenuButton.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using System.IO;
+using System.Security.Policy;
+using Terraria;
 using TerrariaOverhaul.Core.Configuration;
 
 namespace TerrariaOverhaul.Common.MainMenuOverlays
@@ -9,10 +12,30 @@ namespace TerrariaOverhaul.Common.MainMenuOverlays
 
 		protected override void OnClicked()
 		{
+			/*
 			Process.Start(new ProcessStartInfo(ConfigSystem.ConfigPath) {
 				UseShellExecute = true,
 				Verb = "open",
 			});
+			*/
+
+			string configFilePath = ConfigSystem.ConfigPath;
+			string configDirectory = ConfigSystem.ConfigDirectory;
+			string configFileName = Path.GetFileName(configFilePath);
+
+			try {
+				Terraria.Utils.OpenToURL(configFilePath);
+			}
+			catch {
+				try {
+					Terraria.Utils.OpenToURL(configDirectory);
+				}
+				catch {
+					if (Main.menuMode != 888) {
+						Terraria.Utils.ShowFancyErrorMessage($"[c/FF7777:Unable to open Overhaul's configuration file for editing.]\r\nPlease manually navigate to [c/9fecf0:{configDirectory}] and modify [c/9fecf0:{configFileName}] with a text editor of your choosing.\r\n\r\nThe lack of a configuration GUI is temporary.", 0);
+					}
+				}
+			}
 		}
 	}
 }
