5d313931daf9031130c6a7594d5a696da931cf81
Mirsario <me@mirsar.io>
2022-10-20 03:33:20 +0300

    Improved time gradients.

--- Common/Time/TimeGradients.cs
+++ Common/Time/TimeGradients.cs
@@ -6,8 +6,8 @@ public static class TimeGradients
 {
 	public static readonly Gradient<float> Day = new(
 		(TimeOfDay.HoursToTicks(0.00f), 0.00f),
-		(TimeOfDay.HoursToTicks(6.00f), 0.00f),
-		(TimeOfDay.HoursToTicks(9.00f), 1.00f),
+		(TimeOfDay.HoursToTicks(5.00f), 0.00f),
+		(TimeOfDay.HoursToTicks(7.00f), 1.00f),
 		(TimeOfDay.HoursToTicks(17.5f), 1.00f),
 		(TimeOfDay.HoursToTicks(21.0f), 0.00f),
 		(TimeOfDay.HoursToTicks(24.0f), 0.00f)
@@ -15,8 +15,8 @@ public static class TimeGradients
 
 	public static readonly Gradient<float> Night = new(
 		(TimeOfDay.HoursToTicks(0.00f), 1.00f),
-		(TimeOfDay.HoursToTicks(6.00f), 1.00f),
-		(TimeOfDay.HoursToTicks(9.00f), 0.00f),
+		(TimeOfDay.HoursToTicks(5.00f), 1.00f),
+		(TimeOfDay.HoursToTicks(7.00f), 0.00f),
 		(TimeOfDay.HoursToTicks(17.5f), 0.00f),
 		(TimeOfDay.HoursToTicks(21.0f), 1.00f),
 		(TimeOfDay.HoursToTicks(24.0f), 1.00f)
