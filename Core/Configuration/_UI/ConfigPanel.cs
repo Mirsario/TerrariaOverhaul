372b0381b6ade94248a0ccc955b9c321532e0f9f
Mirsario <me@mirsar.io>
2022-11-14 22:32:08 +0300

    Fixes.

--- Core/Configuration/_UI/ConfigPanel.cs
+++ Core/Configuration/_UI/ConfigPanel.cs
@@ -5,23 +5,26 @@ using Terraria.GameContent.UI.Elements;
 using Terraria.ModLoader;
 using Terraria.UI;
 using TerrariaOverhaul.Core.Interface;
-using TerrariaOverhaul.Utilities;
 
 namespace TerrariaOverhaul.Core.Configuration;
 
-[Autoload(Side = ModSide.Client)]
-public class ConfigPanel : UIPanel, ILoadable
+public class ConfigPanel : UIPanel
 {
 	private static Asset<Texture2D> defaultBorderTexture = null!;
 
-	public UIElement Container { get; }
+	public UIElement TitleContainer { get; }
 	public UIImage Thumbnail { get; }
 	public UIPanel ThumbnailContainer { get; }
-	public UIImage? ThumbnailBorder { get; }
+	public UIImage ThumbnailBorder { get; }
 	public UIText Title { get; }
 
 	public ConfigPanel(string title, Asset<Texture2D> thumbnailTexture, Asset<Texture2D>? borderTexture = null) : base()
 	{
+		borderTexture ??= defaultBorderTexture ??= ModContent.Request<Texture2D>($"{nameof(TerrariaOverhaul)}/Core/Configuration/_UI/ThumbnailBorder");
+
+		thumbnailTexture.Wait?.Invoke();
+		borderTexture.Wait?.Invoke();
+
 		// Self
 
 		Width = StyleDimension.FromPixels(135f);
@@ -29,9 +32,9 @@ public class ConfigPanel : UIPanel, ILoadable
 		BorderColor = Color.Black;
 		BackgroundColor = new Color(73, 94, 171);
 
-		// Elements
+		// Thumbnail
 
-		ThumbnailContainer = this.AddElement(new UIPanel().With(static e => {
+		ThumbnailContainer = this.AddElement(new UIPanel().With(e => {
 			e.Width = StyleDimension.FromPixels(112);
 			e.Height = StyleDimension.FromPixels(112f);
 			e.HAlign = 0.5f;
@@ -39,15 +42,21 @@ public class ConfigPanel : UIPanel, ILoadable
 			e.SetPadding(0f);
 		}));
 
-		if (borderTexture != null) {
-			ThumbnailBorder = ThumbnailContainer.AddElement(new UIImage(borderTexture ?? defaultBorderTexture).With(static e => {
-				e.ScaleToFit = true;
-				e.Width = StyleDimension.FromPercent(1f);
-				e.Height = StyleDimension.FromPercent(1f);
-			}));
-		}
+		Thumbnail = ThumbnailContainer.AddElement(new UIImage(thumbnailTexture).With(e => {
+			e.Width = StyleDimension.FromPercent(1f);
+			e.Height = StyleDimension.FromPercent(1f);
+			e.ScaleToFit = true;
+		}));
 
-		Container = this.AddElement(new UIElement().With(static e => {
+		ThumbnailBorder = Thumbnail.AddElement(new UIImage(borderTexture).With(e => {
+			e.ScaleToFit = true;
+			e.Width = StyleDimension.FromPercent(1f);
+			e.Height = StyleDimension.FromPercent(1f);
+		}));
+
+		// Title
+
+		TitleContainer = this.AddElement(new UIElement().With(e => {
 			e.Width = StyleDimension.FromPercent(1f);
 			e.Height = StyleDimension.FromPixels(44f);
 			e.VAlign = 1f;
@@ -55,24 +64,11 @@ public class ConfigPanel : UIPanel, ILoadable
 			e.PaddingBottom = 12f;
 		}));
 
-		Thumbnail = (ThumbnailBorder ?? Container).AddElement(new UIImage(thumbnailTexture).With(static e => {
-			e.Width = StyleDimension.FromPercent(1f);
-			e.Height = StyleDimension.FromPercent(1f);
-			e.ScaleToFit = true;
-		}));
-
-		Title = Container.AddElement(new UIText(title).With(static e => {
+		Title = TitleContainer.AddElement(new UIText(title).With(e => {
 			e.IsWrapped = true;
 			e.Width = StyleDimension.FromPercent(1f);
 			e.Height = StyleDimension.FromPercent(1f);
 			e.VAlign = 0.5f;
 		}));
 	}
-
-	void ILoadable.Load(Mod mod)
-	{
-		defaultBorderTexture = TextureUtils.GetPlaceholderTexture();
-	}
-
-	void ILoadable.Unload() { }
 }
