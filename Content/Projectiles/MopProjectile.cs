a823a894f99f681147ad1d856d8cf0e926f49aee
Mirsario <me@mirsar.io>
2022-12-01 03:29:21 +0300

    Made the mop use a texture for decal clearing.

--- Content/Projectiles/MopProjectile.cs
+++ Content/Projectiles/MopProjectile.cs
@@ -1,13 +1,24 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
 using TerrariaOverhaul.Common.BloodAndGore;
 using TerrariaOverhaul.Common.Decals;
+using TerrariaOverhaul.Utilities;
 
 namespace TerrariaOverhaul.Content.Projectiles;
 
 public class MopProjectile : SpearProjectileBase
 {
+	private static Asset<Texture2D>? clearingDecalTexture;
+
 	protected override float HoldoutRangeMin => 40f;
 	protected override float HoldoutRangeMax => 80f;
+
+	public override void Load()
+	{
+		clearingDecalTexture = Mod.Assets.Request<Texture2D>("Assets/Textures/CleaningDecal");
+	}
 
 	public override void SetDefaults()
 	{
@@ -28,8 +39,19 @@ public class MopProjectile : SpearProjectileBase
 	{
 		base.PostAI();
 
-		if (!Main.dedServ) {
-			DecalSystem.ClearDecals(Projectile.getRect());
+		if (Main.dedServ || clearingDecalTexture?.IsLoaded != true) {
+			return;
 		}
+
+		var center = (Vector2Int)Projectile.Center;
+		var clearSize = new Vector2Int(48, 48);
+		var clearRectangle = new Rectangle(
+			center.X - clearSize.X / 2,
+			center.Y - clearSize.Y / 2,
+			clearSize.X,
+			clearSize.Y
+		);
+
+		DecalSystem.ClearDecals(clearingDecalTexture.Value, clearRectangle, Color.White);
 	}
 }
