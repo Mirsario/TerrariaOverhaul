f7bc33181e83faf9a5a7b800a0be2bb2315283fe
Mirsario <me@mirsar.io>
2022-12-09 01:18:27 +0300

    Don't smooth out teleportation.

--- Common/Camera/SmoothCameraSystem.cs
+++ Common/Camera/SmoothCameraSystem.cs
@@ -4,6 +4,7 @@ using Terraria;
 using Terraria.ModLoader;
 using TerrariaOverhaul.Core.Configuration;
 using TerrariaOverhaul.Core.Time;
+using TerrariaOverhaul.Utilities;
 
 namespace TerrariaOverhaul.Common.Camera;
 
@@ -27,6 +28,9 @@ public sealed class SmoothCameraSystem : ModSystem
 			innerAction();
 
 			var newPosition = Main.screenPosition;
+			var difference = newPosition - oldPosition.Value;
+			float differenceLength = difference.SafeLength();
+			float maxDifferenceLength = new Vector2(Main.screenWidth, Main.screenHeight).SafeLength() * 0.5f + 100f;
 
 #if DEBUG
 			/*
@@ -34,7 +38,7 @@ public sealed class SmoothCameraSystem : ModSystem
 			*/
 #endif
 
-			if (CameraSmoothness > 0f) {
+			if (CameraSmoothness > 0f && differenceLength < maxDifferenceLength) {
 				const float BaseSmoothness = 0.01f;
 
 				float deltaTime = CameraSystem.LimitCameraUpdateRate ? TimeSystem.LogicDeltaTime : TimeSystem.RenderDeltaTime;
