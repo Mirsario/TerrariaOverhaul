59c1b77a394d99703c33bdf790bec888eda0e205
Mirsario <me@mirsar.io>
2022-12-23 07:46:33 +0300

    Reworked melee air combat around movement input.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -42,10 +42,14 @@
 
 ### Changes
 - Rewrote and improved everything about the camera features - [#168](https://github.com/Mirsario/TerrariaOverhaul/pull/168).
-- Heavily improved melee swing velocity - [#137](https://github.com/Mirsario/TerrariaOverhaul/pull/137) (and many commits after that.)
+- Heavily improved melee swing velocity, now respects player movement input - [#137](https://github.com/Mirsario/TerrariaOverhaul/pull/137) (and many commits after that.)
+- Enemy velocity no longer stacks indefinitely from knockback. Fixes shotguns launching demon eyes to space.
+- Melee air combat completely reworked:
+	- Now uses player movement input in place of attack direction. This is much more predictable, natural, and leaves the player in control.
+	- No longer reduces knockback done to enemies while mid air, which had to be done due to the aforementioned knockback stacking.
+	- The above changes result in the players now being able to keep themselves above enemies even by striking downwards, or even push themselves away from the enemies they're attacking. Experiment!
 - Added Bunnyhop Combos, reworked Bunny Paw - [#140](https://github.com/Mirsario/TerrariaOverhaul/pull/140).
 - New health & mana pickup sounds - Closes [#113](https://github.com/Mirsario/TerrariaOverhaul/issues/113).
-- Enemy velocity no longer stacks indefinitely from knockback. Fixes shotguns launching demon eyes to space.
 - Heavily improved life & mana pickup visuals.
 - Ambience: Improved bird & cricket audio.
 - Ambience: Bird audio is now tied to the amount of nearby trees.
--- Common/Melee/ItemMeleeAirCombat.cs
+++ Common/Melee/ItemMeleeAirCombat.cs
@@ -11,18 +11,13 @@ namespace TerrariaOverhaul.Common.Melee;
 public sealed class ItemMeleeAirCombat : ItemComponent
 {
 	public static readonly ConfigEntry<bool> EnableAirCombat = new(ConfigSide.Both, "Melee", nameof(EnableAirCombat), () => true);
-	
-	public override void ModifyHitNPC(Item item, Player player, NPC target, ref int damage, ref float knockback, ref bool crit)
-	{
-		if (!Enabled || !item.TryGetGlobalItem<ItemMeleeAttackAiming>(out var meleeAttackAiming)) {
-			return;
-		}
 
-		// Reduce knockback when the player is in air, and the enemy is somewhat above them.
-		if (!player.OnGround() && meleeAttackAiming.AttackDirection.Y < 0.25f) {
-			knockback *= 0.75f;
-		}
-	}
+	public Vector2 MinAddedSpeed { get; set; } = Vector2.One * 2.0f;
+	public Vector2 MaxAddedSpeed { get; set; } = Vector2.One * 9.0f;
+	public Vector2 HardVelocityCap { get; set; } = new Vector2(18.0f, 18.0f);
+	public Vector2 FixedVelocityBonus { get; set; } = new Vector2(0.0f, -2.0f);
+	public MovementModifier MovementModifier { get; set; } = new() { GravityScale = 0.1f };
+	public float MovementModifierLengthMultiplier { get; set; } = 0.5f;
 
 	public override void OnHitNPC(Item item, Player player, NPC target, int damage, float knockback, bool crit)
 	{
@@ -31,36 +26,39 @@ public sealed class ItemMeleeAirCombat : ItemComponent
 		}
 
 		var movement = player.GetModPlayer<PlayerMovement>();
-		var modifier = PlayerMovement.MovementModifier.Default;
+		var keyDirection = player.KeyDirection();
 
-		if (player.velocity.Y != 0f) {
-			if (meleeAttackAiming.AttackDirection.Y < 0.1f) {
-				modifier.GravityScale *= 0.1f;
-			}
-
+		if (player.velocity.Y != 0f && keyDirection != default) {
 			var positionDifference = target.Center - player.Center;
 			float distance = positionDifference.SafeLength();
-			var dashDirection = target.velocity.SafeNormalize(default);
+
+			// Apply movement modifier.
+
+			int modifierLength = (int)MathF.Floor(player.itemAnimationMax * MovementModifierLengthMultiplier);
+
+			movement.SetMovementModifier($"{nameof(ItemMeleeAirCombat)}/{nameof(OnHitNPC)}", modifierLength, MovementModifier);
+
+			var dashDirection = keyDirection.SafeNormalize(-Vector2.UnitY);
 			var dashVelocity = dashDirection;
 
-			// Boost velocity is based on item knockback.
+			// Boost velocity is based on enemy's current speed.
+
 			float targetSpeed = target.velocity.SafeLength();
+			var targetVelocityDirection = target.velocity.SafeNormalize(-Vector2.UnitY);
+			float dashDirectionVsTargetVelocityDirectionDot = (Vector2.Dot(dashDirection, targetVelocityDirection) + 1f) * 0.5f;
+			float adjustedTargetSpeed = targetSpeed * dashDirectionVsTargetVelocityDirectionDot;
+			Vector2 dashSpeeds = Vector2.Min(Vector2.Max(MinAddedSpeed, Vector2.One * adjustedTargetSpeed), MaxAddedSpeed);
 
-			dashVelocity *= Math.Min(Math.Max(2f, targetSpeed), distance / 3f);
+			dashVelocity *= dashSpeeds;
 
-			// Reduce intensity when the player is not directly aiming at the enemy.
-			float directionsDotProduct = Vector2.Dot(dashDirection, meleeAttackAiming.AttackDirection);
+			// Apply fixed velocity bonuses.
+			dashVelocity += FixedVelocityBonus;
 
-			dashVelocity *= Math.Max(0f, directionsDotProduct * directionsDotProduct);
+			// Add the velocity!
 
-			// Slight upwards movement bonus.
-			dashVelocity.Y -= 1.5f;
-
-			var maxVelocity = Vector2.Min(Vector2.One * 9f, new Vector2(Math.Abs(dashVelocity.X), Math.Abs(dashVelocity.Y)));
+			var maxVelocity = Vector2.Min(HardVelocityCap, new Vector2(Math.Abs(dashVelocity.X), Math.Abs(dashVelocity.Y)));
 
 			VelocityUtils.AddLimitedVelocity(player, dashVelocity, maxVelocity);
 		}
-
-		movement.SetMovementModifier($"{nameof(ItemMeleeAirCombat)}/{nameof(OnHitNPC)}", player.itemAnimationMax / 2, modifier);
 	}
 }
