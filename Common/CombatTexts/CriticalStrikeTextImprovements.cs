abf328b5efbbb2d834d9985f6369bc0cc90c3d45
Mirsario <me@mirsar.io>
2022-12-24 05:37:55 +0300

    Add exclamation point to all crit texts.

---
+++ Common/CombatTexts/CriticalStrikeTextImprovements.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaOverhaul.Common.CombatTexts;
+
+public sealed class CriticalStrikeTextImprovements : ModSystem
+{
+	public override void Load()
+	{
+		On.Terraria.NPC.StrikeNPC += NPC_StrikeNPC;
+	}
+
+	private static double NPC_StrikeNPC(On.Terraria.NPC.orig_StrikeNPC orig, NPC self, int damage, float knockback, int hitDirection, bool crit, bool noEffect, bool fromNet)
+	{
+		if (crit) {
+			CombatTextSystem.AddFilter(1, text => {
+				if (uint.TryParse(text.text, out _) && !text.text.Contains('!')) {
+					text.text += "!";
+				}
+			});
+		}
+
+		return orig(self, damage, knockback, hitDirection, crit, noEffect, fromNet);
+	}
+}
