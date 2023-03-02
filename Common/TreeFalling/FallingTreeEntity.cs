a6d3a5817023b1f78938290e45a5e9bd015ca000
Mirsario <me@mirsar.io>
2023-03-02 18:48:15 +0300

    WIP falling tree reimplementation.

---
+++ Common/TreeFalling/FallingTreeEntity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using TerrariaOverhaul.Core.SimpleEntities;
+using TerrariaOverhaul.Core.Time;
+using TerrariaOverhaul.Utilities;
+
+namespace TerrariaOverhaul.Common.TreeFalling;
+
+public sealed class FallingTreeEntity : SimpleEntity
+{
+	public int TreeHeight;
+	public float Rotation;
+	public Vector2 Position;
+	public Vector2 TextureOrigin;
+	public RenderTarget2D? Texture;
+	public bool IsTextureDisposable = true;
+
+	public override void Init()
+	{
+		// Lame design.
+		if (Texture == null) {
+			throw new InvalidOperationException($"{nameof(Texture)} property must be assigned during pre-initialization.");
+		}
+	}
+
+	public override void Update()
+	{
+		if (TileCollisionCheck() || MathF.Abs(Rotation) >= MathHelper.Pi) {
+			Destroy(allowEffects: true);
+		}
+
+		float rotationSpeed = MathHelper.Lerp(2.5f, 110f, MathUtils.Clamp01(MathF.Abs(Rotation / MathHelper.PiOver2)));
+
+		Rotation += MathHelper.ToRadians(rotationSpeed * TimeSystem.LogicDeltaTime);
+	}
+
+	public override void Draw(SpriteBatch sb)
+	{
+		if (Texture?.IsDisposed != false) {
+			return;
+		}
+
+		var color = Lighting.GetColor(Position.ToTileCoordinates());
+
+		sb.Draw(Texture, Position - Main.screenPosition, null, color, Rotation, TextureOrigin, 1.0f, SpriteEffects.None, 0f);
+	}
+
+	protected override void OnDestroyed(bool allowEffects)
+	{
+		if (IsTextureDisposable && Texture is { IsDisposed: false }) {
+			Texture.Dispose();
+			Texture = null;
+		}
+	}
+
+	private bool TileCollisionCheck()
+	{
+		var position = Position;
+		float rotation = Rotation;
+
+		for (int i = 0; i < TreeHeight; i++) {
+			var positionOffset = new Vector2(0f, -i * TileUtils.TileSizeInPixels);
+			var transformedOffset = positionOffset.RotatedBy(rotation);
+
+			var offsetPosition = position + transformedOffset;
+			var tilePosition = offsetPosition.ToTileCoordinates();
+
+			if (!Main.tile.TryGet(tilePosition, out var tile)) {
+				continue;
+			}
+
+			if (tile.HasUnactuatedTile && Main.tileSolid[tile.TileType]) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
---
+++ Common/TreeFalling/TreeFallingSystem.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Core.Configuration;
+using TerrariaOverhaul.Core.SimpleEntities;
+using TerrariaOverhaul.Core.Tiles;
+using TerrariaOverhaul.Utilities;
+using On_WorldGen = On.Terraria.WorldGen;
+
+namespace TerrariaOverhaul.Common.TreeFalling;
+
+[Autoload(Side = ModSide.Client)]
+public sealed class TreeFallingSystem : ModSystem
+{
+	public static readonly ConfigEntry<bool> EnableTreeFallingAnimations = new(ConfigSide.Both, "Aesthetics", nameof(EnableTreeFallingAnimations), () => true);
+
+	private static bool isTreeBeingDestroyed;
+
+	public override void Load()
+	{
+		On_WorldGen.KillTile += KillTileInjection;
+	}
+
+	private static void OnTreeDestroyed(Tile tile, Vector2Int tilePosition)
+	{
+		// Ensure that this is a middle of a tree
+		var bottomPosition = tilePosition + Vector2Int.UnitY;
+		var bottomTile = Main.tile[bottomPosition.X, bottomPosition.Y];
+
+		if (!bottomTile.HasUnactuatedTile || !(IsTreeTile(bottomTile) || Main.tileSolid[bottomTile.TileType])) {
+			return;
+		}
+
+		// Get all adjacent tree tiles above this one
+		var adjacentTreeParts = GetAdjacentTreeParts(tilePosition);
+		var adjacentTreePartsSpan = CollectionsMarshal.AsSpan(adjacentTreeParts);
+
+		// Calculate tilespace AABB
+		CalculateTilesAabb(adjacentTreePartsSpan, out var aabbMin, out var aabbMax);
+
+		int treeHeight = aabbMax.Y - aabbMin.Y + 1;
+		var aabbMinOffset = new Vector2Int(2, 4);
+		var aabbMaxOffset = new Vector2Int(2, 1);
+
+		aabbMin -= aabbMinOffset;
+		aabbMax += aabbMaxOffset;
+
+		// Create tree texture
+		var sizeInTiles = aabbMax - aabbMin + Vector2Int.One;
+		var snapshotTexture = TileSnapshotSystem.CreateSpecificTilesSnapshot(sizeInTiles, aabbMin, adjacentTreePartsSpan);
+
+		// Create tree entity
+		SimpleEntity.Instantiate<FallingTreeEntity>(e => {
+			e.Position = (tilePosition + new Vector2(0.5f, aabbMaxOffset.Y)) * TileUtils.TileSizeInPixels;
+			e.TreeHeight = treeHeight;
+
+			e.IsTextureDisposable = true;
+			e.Texture = snapshotTexture;
+			e.TextureOrigin = new Vector2(
+				(tilePosition.X - aabbMin.X + 0.5f) * TileUtils.TileSizeInPixels,
+				snapshotTexture.Height - (aabbMaxOffset.Y * TileUtils.TileSizeInPixels)
+			);
+		});
+	}
+
+	private static List<Vector2Int> GetAdjacentTreeParts(Vector2Int basePosition)
+	{
+		var tilePositions = new List<Vector2Int>();
+
+		static bool TileCheck(Tile tile)
+			=> tile.HasUnactuatedTile && IsTreeTile(tile);
+
+		for (var checkPosition = basePosition; checkPosition.Y >= 0; checkPosition.Y--) {
+			var checkTile = Main.tile[checkPosition.X, checkPosition.Y];
+
+			if (!TileCheck(checkTile)) {
+				break;
+			}
+
+			tilePositions.Add(checkPosition);
+
+			for (int xOffset = -1; xOffset <= 1; xOffset += 2) {
+				var sidePosition = checkPosition + new Vector2Int(xOffset, 0);
+
+				if (Main.tile.TryGet(sidePosition, out Tile sideTile) && TileCheck(sideTile)) {
+					tilePositions.Add(sidePosition);
+				}
+			}
+		}
+
+		return tilePositions;
+	}
+
+	private static void CalculateTilesAabb(ReadOnlySpan<Vector2Int> tilePositions, out Vector2Int aabbMin, out Vector2Int aabbMax)
+	{
+		aabbMin = tilePositions[0];
+		aabbMax = tilePositions[0];
+
+		for (int i = 1; i < tilePositions.Length; i++) {
+			var thisPosition = tilePositions[i];
+
+			aabbMin = Vector2Int.Min(aabbMin, thisPosition);
+			aabbMax = Vector2Int.Max(aabbMax, thisPosition);
+		}
+	}
+
+	private static bool IsTreeTile(Tile tile)
+		=> TileID.Sets.IsATreeTrunk[tile.TileType];
+
+	private static void KillTileInjection(On_WorldGen.orig_KillTile original, int x, int y, bool fail, bool effectOnly, bool noItem)
+	{
+		var tile = Main.tile[x, y];
+		var tilePosition = new Vector2Int(x, y);
+		bool wasTreeBeingDestroyed = isTreeBeingDestroyed;
+
+		if (!Main.dedServ && !fail && EnableTreeFallingAnimations && IsTreeTile(tile) && !isTreeBeingDestroyed) {
+			OnTreeDestroyed(tile, tilePosition);
+
+			isTreeBeingDestroyed = true;
+		}
+
+		original(x, y, fail, effectOnly, noItem);
+
+		isTreeBeingDestroyed = wasTreeBeingDestroyed;
+	}
+}
