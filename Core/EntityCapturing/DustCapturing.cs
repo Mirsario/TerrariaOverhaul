1f08d5fadda46479b1c71c77c539d9aad5749707
Mirsario <me@mirsar.io>
2023-03-07 03:29:01 +0300

    Entity spawn hijacking utilities.

---
+++ Core/EntityCapturing/DustCapturing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Utilities;
+using On_Dust = On.Terraria.Dust;
+
+namespace TerrariaOverhaul.Core.EntityCapturing;
+
+public readonly record struct DustCapture(int Type, Vector2 Position, Vector2 Velocity, int Alpha, Color NewColor, float Scale);
+
+public sealed class DustCapturing : ModSystem
+{
+	private static readonly Stack<List<DustCapture>> listStack = new();
+	private static readonly Ref<uint> skipCounter = new();
+
+	public override void Load()
+	{
+		On_Dust.NewDust += NewDustDetour;
+	}
+
+	public static CaptureHandle<DustCapture> Capture(List<DustCapture> captures)
+		=> new(listStack, captures);
+
+	public static CaptureHandle<DustCapture> Capture(out List<DustCapture> captures)
+		=> Capture(captures = new());
+
+	public static CounterHandle Suspend()
+		=> new(skipCounter);
+
+	private static int NewDustDetour(On_Dust.orig_NewDust orig, Vector2 position, int width, int height, int type, float speedX, float speedY, int alpha, Color newColor, float scale)
+	{
+		if (listStack.TryPeek(out var list) && skipCounter.Value == 0) {
+			var randomPosition = Main.rand.NextVector2(position.X, position.Y, position.X + width, position.Y + height);
+			var velocity = new Vector2(speedX, speedY);
+
+			list.Add(new DustCapture(type, randomPosition, velocity, alpha, newColor, scale));
+
+			return Main.maxItems;
+		}
+
+		return orig(position, width, height, type, speedX, speedY, alpha, newColor, scale);
+	}
+}
---
+++ Core/EntityCapturing/ItemCapturing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Utilities;
+using On_Item = On.Terraria.Item;
+
+namespace TerrariaOverhaul.Core.EntityCapturing;
+
+public readonly record struct ItemCapture(IEntitySource Source, Vector2 Position, int Type, int Stack, int Prefix);
+
+public sealed class ItemCapturing : ModSystem
+{
+	private static readonly Stack<List<ItemCapture>> listStack = new();
+	private static readonly Ref<uint> skipCounter = new();
+
+	public override void Load()
+	{
+		On_Item.NewItem_IEntitySource_int_int_int_int_int_int_bool_int_bool_bool += NewItemDetour;
+	}
+
+	public static CaptureHandle<ItemCapture> Capture(List<ItemCapture> captures)
+		=> new(listStack, captures);
+
+	public static CaptureHandle<ItemCapture> Capture(out List<ItemCapture> captures)
+		=> Capture(captures = new());
+
+	public static CounterHandle Suspend()
+		=> new(skipCounter);
+
+	private static int NewItemDetour(On_Item.orig_NewItem_IEntitySource_int_int_int_int_int_int_bool_int_bool_bool orig, IEntitySource source, int x, int y, int width, int height, int type, int stack, bool noBroadcast, int prefix, bool noGrabDelay, bool reverseLookup)
+	{
+		if (listStack.TryPeek(out var list) && skipCounter.Value == 0) {
+			list.Add(new ItemCapture(source, Main.rand.NextVector2(x, y, x + width, y + height), type, stack, prefix));
+
+			return Main.maxItems;
+		}
+
+		return orig(source, x, y, width, height, type, stack, noBroadcast, prefix, noGrabDelay, reverseLookup);
+	}
+}
