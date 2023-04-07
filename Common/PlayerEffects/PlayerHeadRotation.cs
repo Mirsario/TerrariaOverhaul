fb50d65b3a13a5dbb12b37c4e30e917b8029fc68
Mirsario <me@mirsar.io>
2023-04-07 13:08:05 +0300

    Dynamic player gore generation (#179)
    * Dynamically textured gore system.
    
    * Limit player head rotation to vanilla drawcalls.
    
    * Handle Gore.Frame in dynamic gore rendering.
    
    * Implemented payer gore system.
    
    * Fixed DynamicGore.Unload not running on main thread.
    
    * Perfected mask enumeration performance.
    
    * Offsets for gore spawns.
    
    * Disabled rendertarget output define.
    
    * Changelog entry.
    
    * Fixed dye, accessory, and robe rendering.
    
    * Cleanup.

--- Common/PlayerEffects/PlayerHeadRotation.cs
+++ Common/PlayerEffects/PlayerHeadRotation.cs
@@ -13,8 +13,30 @@ public class PlayerHeadRotation : ModPlayer
 {
 	public static readonly ConfigEntry<bool> EnablePlayerHeadRotation = new(ConfigSide.ClientOnly, "PlayerVisuals", nameof(EnablePlayerHeadRotation), () => true);
 
+	private static bool active;
+
 	private float headRotation;
 	private float targetHeadRotation;
+
+	public override void Load()
+	{
+		// The effect is exclusive to vanilla call paths, so that it doesn't screw with mods' custom draw calls.
+		On.Terraria.Main.DrawPlayers_AfterProjectiles += static (orig, main) => {
+			active = true;
+
+			orig(main);
+
+			active = false;
+		};
+
+		On.Terraria.Main.DrawPlayers_BehindNPCs += static (orig, main) => {
+			active = true;
+
+			orig(main);
+
+			active = false;
+		};
+	}
 
 	public override void PreUpdate()
 	{
@@ -38,8 +60,8 @@ public class PlayerHeadRotation : ModPlayer
 
 	public override void ModifyDrawInfo(ref PlayerDrawSet drawInfo)
 	{
-		if (!Main.gameMenu && EnablePlayerHeadRotation) {
-			Player.headRotation = headRotation;
+		if (!Main.gameMenu && EnablePlayerHeadRotation && active) {
+			drawInfo.drawPlayer.headRotation = headRotation;
 		}
 	}
 }
