9b707f736835cbea583a7d75e4ccdddea92ad9cf
Mirsario <me@mirsar.io>
2023-06-26 04:04:17 +0300

    Reimplemented entity water & rain interactions.

---
+++ Common/Interaction/NpcWaterInteraction.cs
@@ -0,0 +1,45 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Common.Camera;
+using TerrariaOverhaul.Utilities;
+using static TerrariaOverhaul.Common.Interaction.WaterAndRainInteractions;
+
+namespace TerrariaOverhaul.Common.Interaction;
+
+public sealed class NpcWaterInteraction : GlobalNPC
+{
+	public override void Load()
+	{
+		On_NPC.UpdateNPC_BuffApplyVFX += NpcVfxDetour;
+	}
+
+	public override void PostAI(NPC npc)
+	{
+		bool ThresholdCheck()
+		{
+			int debuffIndex = npc.FindBuffIndex(DebuffId);
+
+			return debuffIndex < 0 || npc.buffTime[debuffIndex] <= DebuffTimeThreshold;
+		}
+
+		if (Main.netMode == NetmodeID.MultiplayerClient || !LiquidCheck(npc)) {
+			// Rain collision checks only run on clients, for on-screen NPCs.
+			if (Main.netMode == NetmodeID.Server || !CameraSystem.ScreenRect.Intersects(npc.GetRectangle()) || !ThresholdCheck() || !RainCheck(npc)) {
+				return;
+			}
+		} else if (!ThresholdCheck()) {
+			return;
+		}
+
+		npc.AddBuff(DebuffId, DebuffTime, quiet: false);
+	}
+
+	// Fix for dripping particles being spawned while underwater
+	private static void NpcVfxDetour(On_NPC.orig_UpdateNPC_BuffApplyVFX original, NPC npc)
+	{
+		npc.dripping &= !npc.wet;
+
+		original(npc);
+	}
+}
