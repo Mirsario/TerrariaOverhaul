866d79c7f474589cf6aebdf3ef174f0977bef513
Mirsario <me@mirsar.io>
2023-07-25 16:54:18 +0300

    Rewrote hover shots with input grace periods.

--- Common/Archery/ItemPowerAttackHover.cs
+++ Common/Archery/ItemPowerAttackHover.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Terraria;
 using TerrariaOverhaul.Common.Charging;
 using TerrariaOverhaul.Common.Items;
 using TerrariaOverhaul.Common.Movement;
 using TerrariaOverhaul.Common.PlayerEffects;
 using TerrariaOverhaul.Core.ItemComponents;
+using TerrariaOverhaul.Core.Time;
 using TerrariaOverhaul.Utilities;
 
 namespace TerrariaOverhaul.Common.Archery;
 
-//TODO: Add more grace-timing to everything.
-//TODO: Disallow hovering 1 pixel above the ground.
+//TODO: Disallow hovering few pixels above the ground.
 public sealed class ItemPowerAttackHover : ItemComponent
 {
+	private ref struct Context
+	{
+		public Item Item;
+		public Player Player;
+		public ItemPowerAttacks PowerAttacks;
+		public PlayerMovement Movement;
+		public ulong UpdateCount;
+	}
+
 	private static readonly Gradient<float> activationGradient = new(stackalloc Gradient<float>.Key[] {
 		(0.00f, 0.0f),
 		(0.25f, 1.0f),
@@ -27,9 +35,13 @@ public sealed class ItemPowerAttackHover : ItemComponent
 		RunAccelerationScale = 0.50f,
 		VelocityScale = (Positive: new Vector2(1.00f, 0.10f), Negative: Vector2.One),
 	};
+	public uint InputsGracePeriod = 10;
+	public uint RecoilGracePeriod = 3;
 
 	private bool active;
 	private bool needsGroundReset;
+	private uint graceTicks;
+	private ulong lastActiveTime;
 
 	public override void HoldItem(Item item, Player player)
 	{
@@ -37,86 +49,108 @@ public sealed class ItemPowerAttackHover : ItemComponent
 			return;
 		}
 
-		// Player has to be mid-air.
-		if (player.velocity.Y == 0f) {
-			Stop(item, onGround: true);
-			return;
-		}
-
-		if (needsGroundReset) {
-			return;
-		}
-
-		// Player has to be holding the jump button and not be holding down.
-		if (!player.controlJump || player.controlDown) {
-			Stop(item);
-			return;
-		}
-
-		// If outside required velocity range when starting - cease.
-		if (!active && ActivationVelocityRange is Vector4 range && player.velocity is Vector2 velocity) {
-			if (velocity.X < range.X || velocity.X > range.Z || velocity.Y < range.Y || velocity.Y > range.W) {
-				Stop(item);
-				return;
-			}
-		}
+		var c = new Context {
+			Item = item,
+			Player = player,
+			UpdateCount = TimeSystem.UpdateCount,
+		};
 
 		// Acquire necessary components.
-		if (!item.TryGetGlobalItem(out ItemPowerAttacks powerAttacks) || !player.TryGetModPlayer(out PlayerMovement movement)) {
+		if (!item.TryGetGlobalItem(out c.PowerAttacks) || !player.TryGetModPlayer(out c.Movement)) {
 			return;
+		}
+
+		// Check conditions & run activation/deactivation.
+		active = CheckConditions(in c);
+
+		if (active) {
+			lastActiveTime = c.UpdateCount;
+
+			// Execute hovering
+			Hover(in c);
+		}
+
+		if (ControlsVelocityRecoil && c.Item.TryGetGlobalItem(out ItemUseVelocityRecoil velocityRecoil)) {
+			bool velocityRecoilActive = c.UpdateCount - lastActiveTime <= InputsGracePeriod;
+
+			velocityRecoil.SetEnabled(c.Item, velocityRecoilActive);
+		}
+	}
+
+	private bool CheckConditions(in Context c)
+	{
+		// Player has to be mid-air.
+		if (c.Player.OnGround()) {
+			needsGroundReset = false;
 		}
 
 		// Must be charging a power attack.
-		if (!powerAttacks.Enabled || !powerAttacks.IsCharging) {
-			Stop(item);
-			return;
+		if (!c.PowerAttacks.Enabled || !c.PowerAttacks.IsCharging) {
+			graceTicks = 0;
+			return false;
 		}
 
-		if (!active) {
-			Start(item);
+		// If outside required velocity range when starting - cease.
+		if (!active && ActivationVelocityRange is Vector4 range && c.Player.velocity is Vector2 velocity) {
+			if (velocity.X < range.X || velocity.X > range.Z || velocity.Y < range.Y || velocity.Y > range.W) {
+				return false;
+			}
 		}
 
-		float progressFactor = activationGradient.GetValue(powerAttacks.Charge.Progress);
+		if (graceTicks > InputsGracePeriod) {
+			if (!c.Player.OnGround()) {
+				needsGroundReset = true;
+			}
+
+			return false;
+		}
+
+		if (!CheckGraceableConditions(in c)) {
+			graceTicks++;
+			return false;
+		}
+
+		graceTicks = 0;
+
+		return true;
+	}
+
+	private bool CheckGraceableConditions(in Context c)
+	{
+		// Player has to be mid-air.
+		if (c.Player.OnGround()) {
+			needsGroundReset = false;
+			return false;
+		}
+
+		// Must not be required to land
+		if (needsGroundReset) {
+			return false;
+		}
+
+		// Player has to be holding the jump button and not be holding down.
+		if (!c.Player.controlJump || c.Player.controlDown) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private void Hover(in Context c)
+	{
+		float progressFactor = activationGradient.GetValue(c.PowerAttacks.Charge.Progress);
 		float factor = progressFactor;
 
 		var modifier = MovementModifier.Lerp(in MovementModifier.Default, in Modifier, factor);
 
-		if (player.velocity.Y >= -0.5f) {
+		if (c.Player.velocity.Y >= -0.5f) {
 			modifier.GravityScale *= 0f;
 		}
 
-		movement.SetMovementModifier(nameof(ItemPowerAttackHover), 2, modifier);
+		c.Movement.SetMovementModifier(nameof(ItemPowerAttackHover), 2, modifier);
 
-		if (!Main.dedServ && player.TryGetModPlayer(out PlayerTrailEffects trailEffects)) {
+		if (!Main.dedServ && c.Player.TryGetModPlayer(out PlayerTrailEffects trailEffects)) {
 			trailEffects.ForceTrailEffect(3);
 		}
-	}
-
-	private void Start(Item item)
-	{
-		if (active) {
-			return;
-		}
-
-		if (ControlsVelocityRecoil && item.TryGetGlobalItem(out ItemUseVelocityRecoil velocityRecoil)) {
-			velocityRecoil.SetEnabled(item, true);
-		}
-
-		active = true;
-	}
-
-	private void Stop(Item item, bool onGround = false)
-	{
-		if (onGround) {
-			needsGroundReset = false;
-		} else if (active) {
-			needsGroundReset = true;
-
-			if (ControlsVelocityRecoil && item.TryGetGlobalItem(out ItemUseVelocityRecoil velocityRecoil)) {
-				velocityRecoil.SetEnabled(item, false);
-			}
-		}
-
-		active = false;
 	}
 }
