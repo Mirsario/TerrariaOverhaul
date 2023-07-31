861bcfd876428bd29d48083a5b31be435a351273
Mirsario <me@mirsar.io>
2023-07-31 21:35:35 +0300

    Players now face NPCs in dialogues (Fixes #124 - Can't pet cats & dogs).
    (cherry picked from commit 9b40da01484d616e76c8989ed4f6c4925994059c)

---
+++ Common/Dialogues/PlayerDialogueDirectioning.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using TerrariaOverhaul.Common.Movement;
+using TerrariaOverhaul.Utilities;
+
+namespace TerrariaOverhaul.Common.Dialogues;
+
+// Forces players to face their conversation partners.
+// This also fixes inability to pet pets.
+public sealed class PlayerDialogueDirectioning : ModPlayer
+{
+	public override void PreUpdate()
+	{
+		if (Player.TalkNPC is NPC { active: true } npc && Player.TryGetModPlayer(out PlayerDirectioning directions)) {
+			var playerCenter = Player.Center;
+			var npcCenter = npc.Center;
+
+			directions.SetDirectionOverride(npcCenter.X > playerCenter.X ? Direction1D.Right : Direction1D.Left, 3);
+			directions.SetLookPositionOverride(npcCenter, 3);
+		}
+	}
+}
